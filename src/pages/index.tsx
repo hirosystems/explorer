f70960749f1427f61b08bbb4b0a5b40260ef68dc
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-03-16 11:11:43 -0500

    refactor: comnponents page and route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,11 @@
-import React from 'react';
-import { Tag, Transaction } from '@components/tags';
-import { Status, Statuses } from '@components/status';
+import * as React from 'react';
+import Link from 'next/link';
+import { Button, Flex } from '@blockstack/ui';
 
-import { Flex, Box, Stack } from '@blockstack/ui';
-
-const TagComponents: React.FC = props => (
-  <Box {...props}>
-    <Stack spacing="loose">
-      {Object.keys(Transaction).map(label => (
-        <Tag type={Transaction[label as keyof typeof Transaction]} />
-      ))}
-    </Stack>
-  </Box>
-);
-
-const StatusComponents: React.FC = props => (
-  <Box {...props}>
-    <Stack spacing="loose">
-      {Object.keys(Statuses).map(label => (
-        <Status status={Statuses[label as keyof typeof Statuses]} />
-      ))}
-    </Stack>
-  </Box>
-);
-
-const Home: React.FC = () => (
+export default () => (
   <Flex height="100vh" width="100%" align="center" justify="center">
-    <Stack isInline>
-      <TagComponents />
-      <StatusComponents />
-    </Stack>
+    <Link href="/components">
+      <Button>Components</Button>
+    </Link>
   </Flex>
 );
-
-export default Home;
