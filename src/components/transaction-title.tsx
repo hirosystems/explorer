8566002965f88da3dd13584855c724324cc7a5b2
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-03-23 14:10:17 -0500

    feat: allow title to take array of types

--- src/components/transaction-title.tsx
+++ src/components/transaction-title.tsx
@@ -6,14 +6,27 @@ import { Box, Stack, Text, BoxProps } from '@blockstack/ui';
 
 interface TitleProps extends BoxProps {
   status: Statuses;
-  type: Transaction;
+  type: Transaction | Transaction[];
 }
+
+const Tags = ({ type, ...rest }: { type: Transaction | Transaction[] }) =>
+  Array.isArray(type) ? (
+    <Box {...rest}>
+      <Stack isInline spacing="tight">
+        {type.map((t: Transaction) => (
+          <Tag type={t} />
+        ))}
+      </Stack>
+    </Box>
+  ) : (
+    <Tag type={type} {...rest} />
+  );
 
 const TitleDetail: React.FC<TitleProps> = ({ status, type, ...rest }) => {
   return (
     <Box {...rest}>
       <Stack isInline spacing="tight">
-        <Tag type={type} />
+        <Tags type={type} />
         <Status status={status} />
       </Stack>
     </Box>
@@ -21,7 +34,7 @@ const TitleDetail: React.FC<TitleProps> = ({ status, type, ...rest }) => {
 };
 
 export const TransactionTitle: React.FC<TitleProps> = ({ status, type, ...rest }) => (
-  <Stack spacing="base-loose" {...rest}>
+  <Stack spacing="base" {...rest}>
     <Box>
       <Text textStyle="display.large" fontSize="36px">
         Transaction details
