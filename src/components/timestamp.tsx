1ec819224f63d00462a284f3f614611caf609b47
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-03-23 14:25:48 -0500

    feat: add timestamp component

---
+++ src/components/timestamp.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { format } from 'timeago.js';
+import { Flex,  Text, FlexProps } from '@blockstack/ui';
+import { ClockIcon } from '@components/svg';
+
+interface TimestampProps extends FlexProps {
+  ts: number;
+}
+
+/**
+ * TODO: make this update if initial time is less than 1 min, so it will render each second out :)
+ */
+export const Timestamp: React.FC<TimestampProps> = ({ ts, ...props }) => {
+  return (
+    <Flex color="ink" align="center" {...props}>
+      <ClockIcon mr="extra-tight" />
+      <Text>{format(ts)}</Text>
+    </Flex>
+  );
+};
