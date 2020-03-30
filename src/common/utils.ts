fadd43c5110ff83b5fcdecfd8a52f6007e6236eb
kyranjamie <kyran.burraston@gmail.com>
2020-03-30 09:27:56 +0200

    feat: add dedupe util

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -2,3 +2,5 @@ import engine from 'store/src/store-engine';
 import lclStorage from 'store/storages/localStorage';
 
 export const store = engine.createStore([lclStorage]);
+
+export const dedupe = (array: string[]) => [...new Set(array)];
