89f34c85dd34cdb59afc01fb425cb0add0ea2292
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-03-31 12:42:54 -0500

    chore: update next.config and deps

--- next.config.js
+++ next.config.js
@@ -1,36 +1,42 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   experimental: {
     modern: true,
     polyfillsOptimization: true,
+    jsconfigPaths: true,
   },
   env: {
     apiServer: 'http://localhost:3999',
   },
-  webpack(config) {
-    const splitChunks = config.optimization && config.optimization.splitChunks;
+  webpack(config, { dev, isServer }) {
+    const splitChunks = config.optimization && config.optimization.splitChunks
+
     if (splitChunks) {
       const cacheGroups = splitChunks.cacheGroups;
       const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
       if (cacheGroups.framework) {
         cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-          test: preactModules,
+          test: preactModules
         });
         cacheGroups.commons.name = 'framework';
-      } else {
+      }
+      else {
         cacheGroups.preact = {
           name: 'commons',
           chunks: 'all',
-          test: preactModules,
+          test: preactModules
         };
       }
     }
-    if (config.resolve.plugins) {
-      config.resolve.plugins.push(new TsconfigPathsPlugin());
-    } else {
-      config.resolve.plugins = [new TsconfigPathsPlugin()];
+
+    // inject Preact DevTools
+    if (dev && !isServer) {
+      const entry = config.entry;
+      config.entry = () => entry().then(entries => {
+        entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
+        return entries;
+      });
     }
+
     return config;
-  },
+  }
 };
