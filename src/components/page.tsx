70f73487bd56c54545509380c1e86c836864ffd8
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-04-06 08:45:27 -0500

    feat: abstract out page elements

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Box } from '@blockstack/ui';
+import { Box, BoxProps } from '@blockstack/ui';
+import { TransactionTitle, TitleProps } from '@components/transaction-title';
+
+export const PageWrapper: React.FC<BoxProps> = props => (
+  <Box pb="extra-loose" mb="extra-loose" px="base" maxWidth="1100px" mx="auto" pt={16} {...props} />
+);
+
+export const PageTop: React.FC<TitleProps> = ({ status, type, ...props }) => (
+  <Box width="100%" {...props}>
+    <TransactionTitle mb="extra-loose" status={status} type={type} />
+  </Box>
+);
 
 export const Page: React.FC = ({ children }) => (
   <Box as="main" maxWidth="1280px" mx="auto">
