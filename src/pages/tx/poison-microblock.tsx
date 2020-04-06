05cc784af24c54c5cd799c14860a3549369defdc
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-04-06 08:48:03 -0500

    feat: poison-microblock page #39

---
+++ src/pages/tx/poison-microblock.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Stack } from '@blockstack/ui';
+
+import { PageTop, PageWrapper } from '@components/page';
+import { Rows } from '@components/rows';
+
+import { TransactionType } from '@models/transaction.interface';
+import { Statuses } from '@components/status';
+import { TransactionDetails } from '@components/transaction-details';
+
+const TransactionPage: React.FC = () => {
+  return (
+    <PageWrapper>
+      <PageTop status={Statuses.PENDING} type={[TransactionType.POISON_MICROBLOCK]} />
+      <Stack shouldWrapChildren spacing="extra-loose">
+        <TransactionDetails hideContract />
+        <Rows
+          items={[
+            {
+              label: {
+                children: 'Header 1',
+              },
+              children: '8a664c1dd163643b40a4e300daffdc1ea6af493d5ee7338ada9396b63b32dedc',
+            },
+            {
+              label: {
+                children: 'Header 2',
+              },
+              children: '8a664c1dd163643b40a4e300daffdc1ea6af493d5ee7338ada9396b63b32dedc',
+            },
+          ]}
+        />
+      </Stack>
+    </PageWrapper>
+  );
+};
+
+export default TransactionPage;
