01d35ee47873a457e5af24613106b507e575220c
kyranjamie <kyran.burraston@gmail.com>
2020-04-14 17:23:10 +0200

    fix: change env var to API_SERVER

--- src/common/api/transactions.tsx
+++ src/common/api/transactions.tsx
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 
 export async function fetchTx({ txid }: { txid: string }) {
-  const resp = await fetch(process.env.apiServer + '/tx/' + txid);
+  const resp = await fetch(process.env.API_SERVER + '/tx/' + txid);
   const transaction = await resp.json();
   return { transaction };
 }
