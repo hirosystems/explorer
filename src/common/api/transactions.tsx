0739a88842770f1214f0b072db637fa1f5af8e16
kyranjamie <kyran.burraston@gmail.com>
2020-04-15 15:19:43 +0200

    feat: pick random tx, remove on launch

--- src/common/api/transactions.tsx
+++ src/common/api/transactions.tsx
@@ -1,7 +1,12 @@
 import fetch from 'isomorphic-unfetch';
+import { Transaction } from '@models/transaction.interface';
 
-export async function fetchTx({ txid }: { txid: string }) {
+export async function fetchTx({ txid }: { txid: string }): Promise<Transaction> {
   const resp = await fetch(process.env.API_SERVER + '/tx/' + txid);
-  const transaction = await resp.json();
-  return { transaction };
+  return await resp.json();
+}
+
+export async function fetchTxList(): Promise<{ results: Transaction[] }> {
+  const resp = await fetch(process.env.API_SERVER + '/tx');
+  return await resp.json();
 }
--- src/store/transactions/actions.ts
+++ src/store/transactions/actions.ts
@@ -1,5 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
+import { Transaction } from '@models/transaction.interface';
 
 export const fetchTransaction = createAction<{ txid: number }>('ADD_TRANSACTION');
-export const fetchTransactionDone = createAction<{ transaction: any }>('ADD_TRANSACTION_DONE');
+export const fetchTransactionDone = createAction<Transaction>('ADD_TRANSACTION_DONE');
 export const fetchTransactionFailed = createAction<{ txid: string }>('ADD_TRANSACTION_FAILED');
