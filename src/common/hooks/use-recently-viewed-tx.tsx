9b59d17a77908ed3061231f7e71d7dbd09562a57
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-04-15 16:17:06 -0500

    fix: dedupe array of txs

--- src/common/hooks/use-recently-viewed-tx.tsx
+++ src/common/hooks/use-recently-viewed-tx.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { store } from '@common/utils';
 import { Transaction } from '@models/transaction.interface';
+import { dedupe } from '@common/utils';
 
 const storageKey = 'recentlyViewedTx';
 
@@ -16,7 +17,7 @@ export const useRecentlyViewedTx = (transaction?: Transaction) => {
     if (transaction) {
       const newTx: CachedTx = { ...transaction, viewedDate: new Date().toISOString() };
       const cachedTxs = viewedTransactions.filter(({ tx_id }) => tx_id !== transaction.tx_id);
-      const newTxList = [...cachedTxs, newTx];
+      const newTxList = dedupe([...cachedTxs, newTx], 'tx_id');
       setViewedTransactions(newTxList);
       store.set(storageKey, newTxList);
     }
