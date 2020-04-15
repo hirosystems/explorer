a8e3fa4eb6dbe04769efd04dfc3fd47c0b0ca12d
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-04-15 10:05:20 -0500

    refactor: use fetch method

--- src/common/api/transactions.tsx
+++ src/common/api/transactions.tsx
@@ -1,12 +1,12 @@
-import fetch from 'isomorphic-unfetch';
 import { Transaction } from '@models/transaction.interface';
+import { fetchFromApi } from '@common/api/fetch';
 
 export async function fetchTx({ txid }: { txid: string }): Promise<Transaction> {
-  const resp = await fetch(process.env.API_SERVER + '/tx/' + txid);
-  return await resp.json();
+  const resp = await fetchFromApi(`/tx/${txid}`);
+  return resp.json();
 }
 
 export async function fetchTxList(): Promise<{ results: Transaction[] }> {
-  const resp = await fetch(process.env.API_SERVER + '/tx');
-  return await resp.json();
+  const resp = await fetchFromApi('/tx');
+  return resp.json();
 }
