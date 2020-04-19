6de611a55cbdba4e813d57ebde74d293e98aa223
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-04-19 14:04:54 -0500

    feat: search with contract name or txid

--- src/common/search.tsx
+++ src/common/search.tsx
@@ -1,22 +1,44 @@
 import * as React from 'react';
 import Router from 'next/router';
-import { validateTxId } from '@common/utils';
+import { validateTxId, validateContractName } from '@common/utils';
 
 export const search = (query: string) => async (e: React.FormEvent) => {
   e.preventDefault();
+
   if (!query.trim().length) {
-    console.warn('No txid provided');
+    console.warn('No query provided');
     return;
   }
+
+  if (query.includes('.') && validateContractName(query)) {
+    await Router.push(
+      {
+        pathname: '/txid/[txid]',
+        query: { txid: query },
+      },
+      `/txid/${query}`
+    );
+    return;
+  }
+
+  if (query.includes('.') && !validateContractName(query)) {
+    console.warn('Invalid contract name provided');
+    return;
+  }
+
+  if (validateTxId(query)) {
+    await Router.push(
+      {
+        pathname: '/txid/[txid]',
+        query: { txid: query },
+      },
+      `/txid/${query}`
+    );
+    return;
+  }
+
   if (!validateTxId(query)) {
     console.warn('Invalid txid provided');
     return;
   }
-  await Router.push(
-    {
-      pathname: '/txid/[txid]',
-      query: { txid: query },
-    },
-    `/txid/${query}`
-  );
 };
--- src/store/transactions/actions.ts
+++ src/store/transactions/actions.ts
@@ -1,8 +1,17 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Transaction } from '@models/transaction.interface';
+import { fetchContract } from '@common/api/contracts';
 import { fetchTx } from '@common/api/transactions';
 
-export const fetchTransaction = createAsyncThunk<Transaction, Transaction['tx_id']>(
+export const fetchTransaction = createAsyncThunk<Transaction, string>(
   'transaction/fetch',
-  async txid => fetchTx(txid)
+  async query => {
+    if (query.includes('.')) {
+      // The user is searching by contract name
+      const { tx_id } = await fetchContract(query);
+      return fetchTx(tx_id);
+    }
+    // searching by a valid id hash
+    return fetchTx(query);
+  }
 );
