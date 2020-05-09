e5678fd8411e560ff81f44d2d1d82d6aabf47fdb
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-05-09 11:49:49 -0500

    feat: choose color mode

--- src/components/color-modes.tsx
+++ src/components/color-modes.tsx
@@ -1,54 +1,91 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { themeGet } from '@styled-system/theme-get';
+import { colorModeStorage, COLOR_MODE_COOKIE } from '@common/utils';
+import { useMediaQuery } from '@common/hooks/use-media-query';
 
 export const colorGet = (path: string, fallback?: string) => themeGet('colors.' + path, fallback);
 
+const colors = (props: any) => ({
+  light: {
+    ['accent']: colorGet('blue')(props),
+    ['bg']: 'white',
+    ['bg-alt']: colorGet('white')(props),
+    ['bg-light']: colorGet('ink.600')(props),
+    ['invert']: colorGet('ink')(props),
+    ['text-hover']: colorGet('blue')(props),
+    ['text-title']: colorGet('ink')(props),
+    ['text-caption']: colorGet('ink.400')(props),
+    ['text-body']: colorGet('ink.900')(props),
+    ['border']: 'rgb(229, 229, 236)',
+    ['feedback-alert']: colorGet('orange')(props),
+    ['feeback-error']: colorGet('red')(props),
+    ['feeback-success']: colorGet('green')(props),
+  },
+  dark: {
+    ['accent']: colorGet('cyan')(props),
+    ['bg']: colorGet('ink')(props),
+    ['bg-alt']: colorGet('ink.800')(props),
+    ['bg-light']: '#2c2e33',
+    ['invert']: 'white',
+    ['text-hover']: colorGet('blue.300')(props),
+    ['text-title']: 'white',
+    ['text-caption']: '#a7a7ad',
+    ['text-body']: colorGet('ink.300')(props),
+    ['border']: 'rgb(39, 41, 46)',
+    ['feedback-alert']: colorGet('orange')(props),
+    ['feeback-error']: colorGet('red')(props),
+    ['feeback-success']: colorGet('green')(props),
+  },
+});
+
+// @ts-ignore
+const colorModeStyles = (props: any) => colors(props)[props.colorMode];
+// @ts-ignore
+const colorMap = (props: any) => Object.keys(colors(props)[props.colorMode]);
+
 export const ColorModes = createGlobalStyle`
-html, body{
-    --colors-bg: white;
-    --colors-bg-alt: ${colorGet('white')};
-    --colors-bg-light: ${colorGet('ink.600')};
-    --colors-invert: ${colorGet('ink')};
-    --colors-text-hover: ${colorGet('blue')};
-    --colors-text-title:  ${colorGet('ink')};
-    --colors-text-caption:  ${colorGet('ink.400')};
-    --colors-text-body:  ${colorGet('ink.900')};
-    --colors-border: rgb(229, 229, 236);
-}
-@media (prefers-color-scheme: dark) {
-  html, body {
-    --colors-bg: ${colorGet('ink')};
-    --colors-bg-light: ${colorGet('ink.800')};
-    --colors-bg-alt: #2C2E33;
-    --colors-invert: white;
-    --colors-text-hover: ${colorGet('blue.300')};
-    --colors-text-title: white;
-    --colors-text-caption: #A7A7AD;
-    --colors-text-body: ${colorGet('ink.300')};
-    --colors-border: rgb(39, 41, 46);
+${({ colorMode = 'light', ...rest }: any) =>
+  colorMap({ colorMode, ...rest }).map(key => {
+    return `--colors-${key}: ${
+      //@ts-ignore
+      colorModeStyles({ colorMode, ...rest })[key as string]
+    };`;
+  })}
+
+  @media (prefers-color-scheme: dark) {
+    html,
+    body {
+    ${({ colorMode = 'dark', ...rest }: any) =>
+      colorMap({ colorMode, ...rest }).map(key => {
+        return `--colors-${key}: ${
+          //@ts-ignore
+          colorModeStyles({ colorMode, ...rest })[key as string]
+        };`;
+      })}
+    }
   }
-}
-@media (prefers-color-scheme: light) {
-  html, body {
-    --colors-bg: white;
-    --colors-bg-alt: ${colorGet('white')};
-    --colors-bg-light: ${colorGet('ink.50')};
-    --colors-invert: ${colorGet('ink')};
-    --colors-text-hover: ${colorGet('blue')};
-    --colors-text-title:  ${colorGet('ink')};
-    --colors-text-caption:  ${colorGet('ink.400')};
-    --colors-text-body:  ${colorGet('ink.900')};
-    --colors-border: rgb(229, 229, 236);
+  
+  @media (prefers-color-scheme: light) {
+    html,
+    body {
+    ${({ colorMode = 'light', ...rest }: any) =>
+      colorMap({ colorMode, ...rest }).map(key => {
+        return `--colors-${key}: ${
+          //@ts-ignore
+          colorModeStyles({ colorMode, ...rest })[key as string]
+        };`;
+      })}
+    }
   }
-}
+    
   html, body, #__next {
     background: var(--colors-bg);
     border-color: var(--colors-border);
   }
-  
+
   input:-webkit-autofill,
-  input:-webkit-autofill:hover, 
+  input:-webkit-autofill:hover,
   input:-webkit-autofill:focus,
   textarea:-webkit-autofill,
   textarea:-webkit-autofill:hover,
@@ -60,4 +97,54 @@ html, body{
     font-size: 16px !important;
     transition: background-color 5000s ease-in-out 0s;
   }
-`;
+  `;
+
+export const ColorModeContext = React.createContext<{ colorMode?: string; toggleColorMode?: any }>({
+  colorMode: undefined,
+});
+
+export const ColorModeProvider = ({
+  colorMode,
+  children,
+}: {
+  colorMode?: string;
+  children: any;
+}) => {
+  const [mode, setMode] = React.useState(colorMode);
+  const [darkmode] = useMediaQuery('(prefers-color-scheme: dark)');
+  const [lightmode] = useMediaQuery('(prefers-color-scheme: light)');
+
+  const setColorMode = useCallback(
+    (mode: 'light' | 'dark') => {
+      setMode(mode);
+      colorModeStorage.set(COLOR_MODE_COOKIE, mode);
+    },
+    [mode]
+  );
+
+  const toggleColorMode = useCallback(() => {
+    if (mode === 'light') {
+      setColorMode('dark');
+      return;
+    }
+    if (mode === 'dark') {
+      setColorMode('light');
+      return;
+    }
+    if (!colorMode && darkmode) {
+      setColorMode('light');
+      return;
+    }
+    if (!mode && lightmode) {
+      setColorMode('dark');
+      return;
+    }
+  }, [mode, lightmode, darkmode]);
+
+  return (
+    <ColorModeContext.Provider value={{ colorMode: mode, toggleColorMode }}>
+      <ColorModes colorMode={mode} />
+      {children}
+    </ColorModeContext.Provider>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,9 @@ import { Provider } from 'react-redux';
 import { createGlobalStyle } from 'styled-components';
 import { RootState, initStore } from '@store';
 import { handleFontLoading } from '@common/fonts';
-import { ColorModes } from '@components/color-modes';
 import { ProgressBar } from '@components/progress-bar';
+import { parseCookies } from 'nookies';
+import { ColorModeProvider } from '@components/color-modes';
 
 const GlobalStyles = createGlobalStyle`
   html, body, #__next {
@@ -19,9 +20,20 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
-class MyApp extends App<ReduxWrapperAppProps<RootState>> {
+interface MyAppProps {
+  colorMode?: 'light' | 'dark';
+}
+// @ts-ignore
+class MyApp extends App<MyAppProps & ReduxWrapperAppProps<RootState>> {
   static async getInitialProps({ Component, ctx }: AppContext) {
     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+    const cookies = parseCookies(ctx);
+    if (cookies && cookies.color_mode) {
+      return {
+        ...pageProps,
+        colorMode: JSON.parse(cookies.color_mode),
+      };
+    }
     return { pageProps };
   }
 
@@ -30,17 +42,16 @@ class MyApp extends App<ReduxWrapperAppProps<RootState>> {
   }
 
   render() {
-    const { Component, pageProps, store } = this.props;
+    const { Component, pageProps, colorMode, store } = this.props;
     return (
       <Provider store={store}>
         <ThemeProvider theme={theme}>
-          <>
+          <ColorModeProvider colorMode={colorMode}>
             <CSSReset />
-            <ColorModes />
             <GlobalStyles />
             <ProgressBar />
             <Component {...pageProps} />
-          </>
+          </ColorModeProvider>
         </ThemeProvider>
       </Provider>
     );
