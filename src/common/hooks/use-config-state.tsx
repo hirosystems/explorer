d5e5d8e2c287ba51a89247258c4c29a35124a5a5
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-05-18 13:56:00 -0500

    fix: only on dev show list items

--- src/common/hooks/use-config-state.tsx
+++ src/common/hooks/use-config-state.tsx
@@ -1,14 +1,20 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '@store';
-import { selectNetworks, selectCurrentNetworkUrl } from '@store/ui/selectors';
+import {
+  selectNetworks,
+  selectCurrentNetworkName,
+  selectCurrentNetworkUrl,
+} from '@store/ui/selectors';
 
 export const useConfigState = () => {
-  const { networks, apiServer } = useSelector((state: RootState) => ({
+  const { networks, selectedNetwork, apiServer } = useSelector((state: RootState) => ({
     networks: selectNetworks(state),
+    selectedNetwork: selectCurrentNetworkName(state),
     apiServer: selectCurrentNetworkUrl(state),
   }));
   return {
     networks,
+    selectedNetwork,
     apiServer,
   };
 };
