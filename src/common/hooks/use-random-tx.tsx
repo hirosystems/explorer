0ffefee36ac2987b57e7f3a6d0a550212a98f481
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-05-18 13:24:39 -0500

    fix: useConfigState hook

--- src/common/hooks/use-random-tx.tsx
+++ src/common/hooks/use-random-tx.tsx
@@ -1,14 +1,12 @@
 import { useCallback } from 'react';
-import { navgiateToRandomTx } from '@common/utils';
+
 import { useToast } from '@common/hooks/use-toast';
-import { useSelector } from 'react-redux';
-import { selectCurrentNetworkUrl } from '@store/ui/selectors';
-import { RootState } from '@store';
+import { useConfigState } from '@common/hooks/use-config-state';
+import { navgiateToRandomTx } from '@common/utils';
 
 export const useNavigateToRandomTx = () => {
-  const { apiServer } = useSelector((state: RootState) => ({
-    apiServer: selectCurrentNetworkUrl(state),
-  }));
+  const { apiServer } = useConfigState();
+
   const { addCriticalToast } = useToast();
   const handleRandomTxClick = useCallback(async () => {
     try {
