318db6154e60b9e612c5488e66a587f1be5efb95
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-05-27 09:45:05 -0500

    feat: add morgan logging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const cacheableResponse = require('cacheable-response');
 const express = require('express');
 const next = require('next');
+const morgan = require('morgan');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -32,6 +33,8 @@ const ssrCache = cacheableResponse({
 
 app.prepare().then(() => {
   const server = express();
+
+  server.use(morgan('combined'));
 
   server.get('/', (req, res) => ssrCache({ req, res, pagePath: '/' }));
 
