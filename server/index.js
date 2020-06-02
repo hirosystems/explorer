9212338445bcaea19cd56c576e704faed9659732
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-06-02 16:18:36 -0500

    build: format morgan logging json

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const cacheableResponse = require('cacheable-response');
 const express = require('express');
 const next = require('next');
-const morgan = require('morgan');
+
+const loggingMiddleware = require('./logging');
+
 const { createMiddleware: createPrometheusMiddleware } = require('@promster/express');
 const { createServer } = require('@promster/server');
 
@@ -11,19 +13,21 @@ const app = next({ dev });
 
 const handle = app.getRequestHandler();
 
+/**
+ * SSR caching
+ */
+
 const ssrCache = cacheableResponse({
   ttl: 1000 * 60 * 5, // 5 minutes
   get: async ({ req, res, pagePath, queryParams }) => {
     const data = await app.renderToHTML(req, res, pagePath, queryParams);
 
-    // Add here custom logic for when you do not want to cache the page, for
-    // example when the page returns a 404 status code:
-    if (res.statusCode === 404) {
+    if (res.statusCode === 404 || res.statusCode === 500) {
       res.end(data);
       return;
     }
 
-    if (pagePath.includes('txid')) {
+    if (pagePath.includes('txid') || pagePath.includes('transactions')) {
       res.end(data);
       return;
     }
@@ -33,15 +37,18 @@ const ssrCache = cacheableResponse({
   send: ({ data, res }) => res.send(data),
 });
 
+/**
+ * App server
+ */
+
 app.prepare().then(() => {
   const server = express();
 
   server.use(createPrometheusMiddleware({ app: server }));
 
-  // Create `/metrics` endpoint on separate server
   if (!dev) {
     createServer({ port: 9153 }).then(() => console.log(`@promster/server started on port 9153.`));
-    server.use(morgan('combined'));
+    server.use(loggingMiddleware());
   }
 
   server.get('/', (req, res) => ssrCache({ req, res, pagePath: '/' }));
