999b2a1a4993e3c06c504ad6307a1a1954c779d4
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-06-02 13:53:37 -0500

    fix: continue

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { Portal } from '@blockstack/ui';
-// import { Tooltip as TooltipBase, TooltipProps } from '@blockstack/ui';
-import BaseTooltip, { useTooltip, TooltipPopup } from '@reach/tooltip';
+import { useTooltip, TooltipPopup } from '@reach/tooltip';
 
 const centered = (triggerRect: any, tooltipRect: any) => {
+  if (typeof window === 'undefined') return { left: 0, top: 0 };
   const triggerCenter = triggerRect.left + triggerRect.width / 2;
   const left = triggerCenter - tooltipRect.width / 2;
   const maxLeft = window.innerWidth - tooltipRect.width - 2;
@@ -12,38 +11,13 @@ const centered = (triggerRect: any, tooltipRect: any) => {
     top: triggerRect.bottom + 8 + window.scrollY,
   };
 };
-export function Tooltip({ children, label, 'aria-label': ariaLabel }: any) {
-  // get the props from useTooltip
+export const Tooltip = ({ children, label, 'aria-label': ariaLabel }: any) => {
   const [trigger, tooltip] = useTooltip();
-
-  // destructure off what we need to position the triangle
-  const { isVisible, triggerRect } = tooltip;
 
   const { onMouseDown, ...triggerProps } = trigger;
   return (
     <>
       {React.cloneElement(children, triggerProps)}
-      {isVisible && (
-        // The Triangle. We position it relative to the trigger, not the popup
-        // so that collisions don't have a triangle pointing off to nowhere.
-        // Using a Portal may seem a little extreme, but we can keep the
-        // positioning logic simpler here instead of needing to consider
-        // the popup's position relative to the trigger and collisions
-        <Portal>
-          <div
-            style={{
-              position: 'absolute',
-              left: triggerRect ? triggerRect.left - 10 + triggerRect.width / 2 : 'unset',
-              top: triggerRect ? triggerRect.bottom + window.scrollY : 'unset',
-              width: 0,
-              height: 0,
-              borderLeft: '10px solid transparent',
-              borderRight: '10px solid transparent',
-              borderBottom: '10px solid black',
-            }}
-          />
-        </Portal>
-      )}
       <TooltipPopup
         {...tooltip}
         label={label}
@@ -61,4 +35,4 @@ export function Tooltip({ children, label, 'aria-label': ariaLabel }: any) {
       />
     </>
   );
-}
+};
