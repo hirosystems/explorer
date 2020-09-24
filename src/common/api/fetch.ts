80bea3c020a89b09b5e0cc2668b7b87ea1827918
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-09-24 11:57:57 -0500

    fix: sidecar -> extended

--- src/common/api/fetch.ts
+++ src/common/api/fetch.ts
@@ -4,7 +4,7 @@ export const fetchFromApi = (apiServer: string) => async (path: string, opts: ob
   fetch(withApiServer(apiServer)(path), opts);
 
 export const fetchFromSidecar = (apiServer: string) => async (path: string, opts: object = {}) =>
-  fetch(withApiServer(apiServer)('/sidecar/v1' + path), opts);
+  fetch(withApiServer(apiServer)('/extended/v1' + path), opts);
 
 export const postToApi = (apiServer: string) => async (path: string, opts: object = {}) =>
   fetchFromApi(apiServer)(path, { method: 'POST', ...opts });
