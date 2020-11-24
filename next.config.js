4b1ba81998aeb312a59f4aec22ff08ece2bda2d0
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-11-24 09:23:48 -0600

    fix: build issues

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const path = require('path');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -16,38 +15,9 @@ module.exports = withBundleAnalyzer({
     TESTNET_API_SERVER: process.env.TESTNET_API_SERVER,
     STAGING: process.env.STAGING,
   },
-  webpack(config, { dev }) {
+  webpack(config) {
     config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
-    if (!dev) {
-      config.externals.unshift('elliptic');
-      config.plugins.push(new webpack.IgnorePlugin(/^\.\/wordlists\/(?!english)/, /bip39\/src$/));
-      const splitChunks = config.optimization && config.optimization.splitChunks;
-      if (splitChunks) {
-        const cacheGroups = splitChunks.cacheGroups;
-        const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-        if (cacheGroups.framework) {
-          cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-            test,
-          });
-          cacheGroups.commons.name = 'framework';
-        } else {
-          cacheGroups.preact = {
-            name: 'commons',
-            chunks: 'all',
-            test,
-          };
-        }
-      }
-
-      // Install webpack aliases:
-      const aliases = config.resolve.alias || (config.resolve.alias = {});
-
-      // aliases.react = aliases['react-dom'] = 'preact/compat';
-      // aliases['react-ssr-prepass'] = 'preact-ssr-prepass';
-      aliases['@blockstack/ui'] = '@stacks/ui';
-      aliases['styled-components'] = '@emotion/react';
-    }
-
+    config.plugins.push(new webpack.IgnorePlugin(/^\.\/wordlists\/(?!english)/, /bip39\/src$/));
     return config;
   },
 });
