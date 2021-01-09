e05e2932df4ce591bcfbf8e41f368468043e9868
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-01-09 13:37:34 -0600

    feat: show vesting data, convert address notice

--- src/common/hooks/use-network-mode.ts
+++ src/common/hooks/use-network-mode.ts
@@ -2,6 +2,6 @@ import { useRecoilValue } from 'recoil';
 import { networkModeState } from '@pages/_app';
 
 export const useNetworkMode = () => {
-  const state = useRecoilValue(networkModeState);
+  const state = useRecoilValue<'Testnet' | 'Mainnet' | null>(networkModeState);
   return state;
 };
