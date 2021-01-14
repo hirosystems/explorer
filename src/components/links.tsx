f8bd4515c516810b2f594d69afd3efa0660f5445
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-01-13 19:17:59 -0600

    fix: meta head fix, address link fix

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -9,6 +9,10 @@ export const BlockLink: React.FC<{ hash: string }> = React.memo(({ hash, ...rest
   <Link href={`/block/${hash}`} passHref {...rest} />
 ));
 
-export const AddressLink: React.FC<{ principal: string }> = React.memo(({ principal, ...rest }) => (
-  <Link href={`/address/${principal}`} passHref {...rest} />
-));
+export const AddressLink: React.FC<{ principal: string }> = React.memo(({ principal, ...rest }) =>
+  principal.includes('.') ? (
+    <TxLink txid={principal} {...rest} />
+  ) : (
+    <Link href={`/address/${principal}`} passHref {...rest} />
+  )
+);
