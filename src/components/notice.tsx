0e037d4c45a002f148d70492eba477bcdcf12142
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-01-15 17:54:55 -0600

    feat: show notice

--- src/components/notice.tsx
+++ src/components/notice.tsx
@@ -8,6 +8,9 @@ export const Notice = ({
   ...rest
 }: { label?: string; message?: string } & FlexProps) => (
   <Box
+    mx="auto"
+    borderRadius="8px"
+    maxWidth="1216px"
     bg="var(--colors-bg-alt)"
     borderBottom="1px solid"
     borderColor="var(--colors-border)"
@@ -15,23 +18,32 @@ export const Notice = ({
   >
     <Flex
       p="tight"
-      alignItems="center"
-      justify="flex-start"
+      alignItems={['flex-start', 'flex-start', 'center']}
+      justifyContent={['center', 'center', 'flex-start']}
       mx="auto"
       width="100%"
       maxWidth="1280px"
-      px={['base', 'base', 'extra-loose']}
+      flexDirection={['column', 'column', 'row']}
+      px={['base']}
     >
-      <Box mr="tight" color="orange">
-        <ExclamationMarkCircleIcon size="14px" />
-      </Box>
-      {label ? (
-        <Text mr="tight" fontWeight="600" fontSize="14px" color="var(--colors-text-title)">
-          {label}
-        </Text>
-      ) : null}
+      <Flex alignItems="center" mb={['tight', 'tight', 'unset']}>
+        <Box mr="tight" color="orange">
+          <ExclamationMarkCircleIcon size="14px" />
+        </Box>
+        {label ? (
+          <Text
+            mr="tight"
+            fontWeight="600"
+            fontSize="14px"
+            lineHeight="22px"
+            color="var(--colors-text-title)"
+          >
+            {label}
+          </Text>
+        ) : null}
+      </Flex>
       {message ? (
-        <Text fontSize="14px" color="var(--colors-text-caption)">
+        <Text fontSize="14px" lineHeight="22px" color="var(--colors-text-body)">
           {message}
         </Text>
       ) : null}
