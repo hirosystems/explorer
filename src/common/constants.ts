ba442b8a35e76734845a0cde817be9404fc7868f
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-01-15 19:10:20 -0600

    fix: try publicRuntimeConfig

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,10 +1,26 @@
 import { ChainID } from '@stacks/transactions';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
+const {
+  NEXT_PUBLIC_DEFAULT_POLLING_INTERVAL,
+  NEXT_PUBLIC_SITE_NOTICE_BANNER_LABEL,
+  NEXT_PUBLIC_SITE_NOTICE_BANNER_MESSAGE,
+  NEXT_PUBLIC_DEPLOYMENT_URL,
+  NEXT_PUBLIC_LEGACY_EXPLORER_API_SERVER,
+  NEXT_PUBLIC_CONNECT_AUTH_ORIGIN,
+  NEXT_PUBLIC_TESTNET_API_SERVER,
+  NEXT_PUBLIC_MAINNET_API_SERVER,
+} = publicRuntimeConfig;
 
 const getNumber = (query?: string): number | undefined =>
   query && typeof parseInt(query) === 'number' ? parseInt(query) : undefined;
 
 export const DEFAULT_POLLING_INTERVAL =
-  getNumber(process.env.NEXT_PUBLIC_DEFAULT_POLLING_INTERVAL) || 10000; // 10 seconds :c
+  getNumber(
+    NEXT_PUBLIC_DEFAULT_POLLING_INTERVAL || process.env.NEXT_PUBLIC_DEFAULT_POLLING_INTERVAL
+  ) || 10000; // 10 seconds :c
 
 export const TESTNET_CHAIN_ID = ChainID.Testnet;
 export const MAINNET_CHAIN_ID = ChainID.Mainnet;
@@ -18,8 +34,10 @@ export const STACKS_EXPLORER_APP_ICON =
 export const DEFAULT_STATUS_ENDPOINT = '/extended/v1/status';
 export const DEFAULT_V2_INFO_ENDPOINT = '/v2/info';
 
-export const SITE_NOTICE_BANNER_LABEL = process.env.NEXT_PUBLIC_SITE_NOTICE_BANNER_LABEL;
-export const SITE_NOTICE_BANNER_MESSAGE = process.env.NEXT_PUBLIC_SITE_NOTICE_BANNER_MESSAGE;
+export const SITE_NOTICE_BANNER_LABEL =
+  NEXT_PUBLIC_SITE_NOTICE_BANNER_LABEL || process.env.NEXT_PUBLIC_SITE_NOTICE_BANNER_LABEL;
+export const SITE_NOTICE_BANNER_MESSAGE =
+  NEXT_PUBLIC_SITE_NOTICE_BANNER_MESSAGE || process.env.NEXT_PUBLIC_SITE_NOTICE_BANNER_MESSAGE;
 export const SITE_NOTICE_ENABLED = SITE_NOTICE_BANNER_LABEL && SITE_NOTICE_BANNER_MESSAGE;
 
 export const APP_DETAILS = {
@@ -27,19 +45,29 @@ export const APP_DETAILS = {
   icon: STACKS_EXPLORER_APP_ICON,
 };
 export const DEPLOYMENT_URL =
-  process.env.NEXT_PUBLIC_DEPLOYMENT_URL || `https://${process.env.VERCEL_URL}`;
+  NEXT_PUBLIC_DEPLOYMENT_URL ||
+  process.env.NEXT_PUBLIC_DEPLOYMENT_URL ||
+  `https://${process.env.VERCEL_URL}`;
 
 export const LEGACY_EXPLORER_API_SERVER =
-  process.env.NEXT_PUBLIC_LEGACY_EXPLORER_API_SERVER || 'https://explorer-api.blockstack.xyz';
+  NEXT_PUBLIC_LEGACY_EXPLORER_API_SERVER ||
+  process.env.NEXT_PUBLIC_LEGACY_EXPLORER_API_SERVER ||
+  'https://explorer-api.blockstack.xyz';
 
 export const CONNECT_AUTH_ORIGIN =
-  process.env.NEXT_PUBLIC_CONNECT_AUTH_ORIGIN || 'https://pr-725.app.stacks.engineering';
+  NEXT_PUBLIC_CONNECT_AUTH_ORIGIN ||
+  process.env.NEXT_PUBLIC_CONNECT_AUTH_ORIGIN ||
+  'https://pr-725.app.stacks.engineering';
 
 export const DEFAULT_TESTNET_SERVER =
-  process.env.NEXT_PUBLIC_TESTNET_API_SERVER || 'https://stacks-node-api.xenon.blockstack.org';
+  NEXT_PUBLIC_TESTNET_API_SERVER ||
+  process.env.NEXT_PUBLIC_TESTNET_API_SERVER ||
+  'https://stacks-node-api.xenon.blockstack.org';
 
 export const DEFAULT_MAINNET_SERVER =
-  process.env.NEXT_PUBLIC_MAINNET_API_SERVER || 'https://stacks-node-api.stacks.co';
+  NEXT_PUBLIC_MAINNET_API_SERVER ||
+  process.env.NEXT_PUBLIC_MAINNET_API_SERVER ||
+  'https://stacks-node-api.stacks.co';
 
 export const MAINNET_ENABLED =
   process.env.NEXT_PUBLIC_MAINNET_ENABLED &&
