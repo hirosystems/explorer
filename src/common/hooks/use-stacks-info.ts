a07bf3490d5dfd51cced03b7a2ceeb652e513e3b
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-01-21 15:22:25 -0600

    fix: remove unused api path

--- src/common/hooks/use-stacks-info.ts
+++ src/common/hooks/use-stacks-info.ts
@@ -1,11 +1,13 @@
 import useSWR from 'swr';
 import { fetchFromApi } from '@common/api/fetch';
-import { DEFAULT_V2_INFO_ENDPOINT, DEPLOYMENT_URL } from '@common/constants';
+import { DEFAULT_V2_INFO_ENDPOINT } from '@common/constants';
+import { useApiServer } from '@common/hooks/use-api';
 
 export const useStacksInfo = (options?: { initialData?: any; suspense?: boolean }) => {
+  const apiServer = useApiServer();
   return useSWR(
     'useStacksInfo',
-    () => fetchFromApi(`${DEPLOYMENT_URL}/api`)(DEFAULT_V2_INFO_ENDPOINT).then(res => res.json()),
+    () => fetchFromApi(apiServer)(DEFAULT_V2_INFO_ENDPOINT).then(res => res.json()),
     {
       initialData: options?.initialData || null,
     }
