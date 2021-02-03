52b7cb435ebe978a11c8c01c0ad7a7eee561748f
Hank Stoever <hstove@gmail.com>
2021-02-03 00:54:48 -0800

    fix: update connect to canary version

--- src/common/sandbox/connect-functions.ts
+++ src/common/sandbox/connect-functions.ts
@@ -6,28 +6,23 @@ import type {
 } from '@stacks/connect';
 import { CONNECT_AUTH_ORIGIN } from '@common/constants';
 
-export const handleStxTransfer = async (options: STXTransferOptions): Promise<Window | null> => {
-  const payload = await openSTXTransfer({
+export const handleStxTransfer = async (options: STXTransferOptions): Promise<void> => {
+  return openSTXTransfer({
     ...options,
     authOrigin: CONNECT_AUTH_ORIGIN,
   });
-  return payload;
 };
 
-export const handleContractCall = async (options: ContractCallOptions): Promise<Window | null> => {
-  const payload = await openContractCall({
+export const handleContractCall = async (options: ContractCallOptions): Promise<void> => {
+  return openContractCall({
     ...options,
     authOrigin: CONNECT_AUTH_ORIGIN,
   });
-  return payload;
 };
 
-export const handleContractDeploy = async (
-  options: ContractDeployOptions
-): Promise<Window | null> => {
-  const payload = await openContractDeploy({
+export const handleContractDeploy = async (options: ContractDeployOptions): Promise<void> => {
+  return openContractDeploy({
     ...options,
     authOrigin: CONNECT_AUTH_ORIGIN,
   });
-  return payload;
 };
