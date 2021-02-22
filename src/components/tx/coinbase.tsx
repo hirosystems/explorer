e47e9ebdb8a3c97dc8185ed1174cf5d50d54da46
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-02-22 12:24:15 -0600

    fix: convert hex log, display events on coinbase, bump deps

--- src/components/tx/coinbase.tsx
+++ src/components/tx/coinbase.tsx
@@ -5,12 +5,17 @@ import { CoinbaseTxs, TxData } from '@common/types/tx';
 import { Block } from '@blockstack/stacks-blockchain-api-types';
 import { PagePanes } from '@components/page-panes';
 import { BtcAnchorBlockCard } from '@components/btc-anchor-card';
+import { Events } from '@components/tx-events';
+import { Box } from '@stacks/ui';
 
 const CoinbasePage = ({ transaction, block }: TxData<CoinbaseTxs> & { block?: Block }) => (
   <>
     <PageTop tx={transaction as any} />
     <PagePanes fullWidth={transaction.tx_status === 'pending' || block === null}>
-      <TransactionDetails transaction={transaction} />
+      <Box>
+        <TransactionDetails transaction={transaction} />
+        {'events' in transaction && <Events mt="extra-loose" events={transaction.events} />}
+      </Box>
       {block && <BtcAnchorBlockCard block={block} />}
     </PagePanes>
   </>
