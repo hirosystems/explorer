49bd243403940b29df2cd938dfeb9101e9fc9b4b
fbwoolf <fwoolf@blockstack.com>
2021-07-22 14:36:16 -0500

    refactor: comments

--- server/index.js
+++ server/index.js
@@ -27,6 +27,30 @@ app.prepare().then(() => {
 
   let routes = [];
 
+  // Store all the registered express routes for usage with metrics reporting
+  routes = expressListEndpoints(app).map(endpoint => ({
+    path: endpoint.path,
+    regexp: pathToRegexp(endpoint.path),
+  }));
+
+  // Manual route definitions for the proxied endpoints
+  routes.push({
+    path: '/txid/*',
+    regexp: /^\/txid(.*)/,
+  });
+  routes.push({
+    path: '/microblock/*',
+    regexp: /^\/microblock(.*)/,
+  });
+  routes.push({
+    path: '/block/*',
+    regexp: /^\/block(.*)/,
+  });
+  routes.push({
+    path: '/address/*',
+    regexp: /^\/address(.*)/,
+  });
+
   if (!dev) {
     const promMiddleware = createPrometheusMiddleware({
       options: {
@@ -34,9 +58,7 @@ app.prepare().then(() => {
           // Get the url pathname without a query string or fragment
           // (note base url doesn't matter, but required by URL constructor)
           let pathTemplate = new URL(path, 'http://x').pathname;
-          // Match request url to the Express route, e.g.:
-          // `/extended/v1/address/ST26DR4VGV507V1RZ1JNM7NN4K3DTGX810S62SBBR/stx` to
-          // `/extended/v1/address/:stx_address/stx`
+          // Match request url to the Express route
           for (const pathRegex of routes) {
             if (pathRegex.regexp.test(pathTemplate)) {
               pathTemplate = pathRegex.path;
@@ -51,30 +73,6 @@ app.prepare().then(() => {
     createServer({ port: 9153 }).then(() => console.log(`@promster/server started on port 9153.`));
     server.use(loggingMiddleware());
   }
-
-  // Store all the registered express routes for usage with metrics reporting
-  routes = expressListEndpoints(app).map(endpoint => ({
-    path: endpoint.path,
-    regexp: pathToRegexp(endpoint.path),
-  }));
-
-  // Manual route definitions for the /v2/ proxied endpoints
-  routes.push({
-    path: '/txid/*',
-    regexp: /^\/txid(.*)/,
-  });
-  routes.push({
-    path: '/tx/*',
-    regexp: /^\/tx(.*)/,
-  });
-  routes.push({
-    path: '/block/*',
-    regexp: /^\/block(.*)/,
-  });
-  routes.push({
-    path: '/address/*',
-    regexp: /^\/address(.*)/,
-  });
 
   server.get('/', (req, res) => {
     // since we don't use next's requestHandler, we lose compression, so we manually add it
