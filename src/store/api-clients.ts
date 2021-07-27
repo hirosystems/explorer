115f18941c1b0084fc4920a45df23949c65c9af4
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-07-27 14:05:56 -0500

    fix: better network url when microblocks enabled

--- src/store/api-clients.ts
+++ src/store/api-clients.ts
@@ -1,19 +1,12 @@
 import { atom } from 'jotai';
 import { Configuration } from '@stacks/blockchain-api-client';
-import {
-  DEFAULT_MAINNET_SERVER,
-  TESTNET_MICROBLOCKS_SERVER,
-  MICROBLOCKS_ENABLED,
-  MAINNET_MICROBLOCKS_SERVER,
-} from '@common/constants';
+import { DEFAULT_MAINNET_SERVER } from '@common/constants';
 import { apiClients, createConfig } from '@common/api/client';
 import { networkUrlState } from '@store/network';
 
 export const apiClientConfiguration = atom<Configuration>(get => {
   const networkUrl = get(networkUrlState); // this should always be defined
-  const apiServer = MICROBLOCKS_ENABLED
-    ? MAINNET_MICROBLOCKS_SERVER
-    : networkUrl || DEFAULT_MAINNET_SERVER;
+  const apiServer = networkUrl || DEFAULT_MAINNET_SERVER;
   return createConfig(apiServer);
 });
 
