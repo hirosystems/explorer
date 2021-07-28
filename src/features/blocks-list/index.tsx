27380be7a3f6efd3860ca9246b29a3ce718cafe9
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-07-28 08:50:54 -0500

    fix: limit blocks + microblocks

--- src/features/blocks-list/index.tsx
+++ src/features/blocks-list/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { color, Flex, FlexProps, Grid, Spinner } from '@stacks/ui';
 import { Section } from '@components/section';
 import { HoverableItem } from '@components/hoverable';
@@ -8,15 +8,31 @@ import { BlockItem } from './block-list-item';
 import { MicroblockItem } from './microblock-list-item';
 import { SafeSuspense } from '@components/ssr-safe-suspense';
 import { SectionFooterAction } from '@components/section-footer-button';
+import { DEFAULT_LIST_LIMIT } from '@common/constants';
 
-export const BlocksList: React.FC<FlexProps & { limit?: number; infinite?: boolean }> = ({
-  infinite,
-  limit,
-  ...props
-}) => {
+export const BlocksList: React.FC<
+  FlexProps & { enforceLimit?: boolean; limit?: number; infinite?: boolean }
+> = ({ infinite, limit = DEFAULT_LIST_LIMIT, enforceLimit, ...props }) => {
   const [blocks, actions] = useBlocksList(limit);
   if (!blocks) return null;
   const hasBlocks = blocks?.pages?.[0]?.results?.length;
+  const firstPage = blocks.pages?.[0].results;
+
+  // due to the way that we fetch from two endpoints (blocks and microblocks)
+  // we need to generate a combined flat list of hashes to know which hashes
+  // fall within a given limit
+  const hashesToShow = useMemo(() => {
+    if (!enforceLimit) return {};
+    const _index: Record<string, number> = {};
+    const flatmap = firstPage
+      .map(block => [block.hash, ...block.microblocks_accepted.map(mb => mb)])
+      .flat(5);
+
+    flatmap.forEach((hash, index) => {
+      _index[hash] = index + 1;
+    });
+    return _index;
+  }, [firstPage, enforceLimit]);
 
   return (
     <Section title="Recent Blocks" {...props}>
@@ -25,10 +41,14 @@ export const BlocksList: React.FC<FlexProps & { limit?: number; infinite?: boole
           <>
             {blocks.pages.map((page, pageIndex, pages) => {
               const isLastPage = pageIndex === pages.length - 1;
+
               return (
                 <>
                   {page?.results.map((block, index, arr) => {
-                    const isLastItem = index === arr.length - 1;
+                    const isLastItem =
+                      index === arr.length - 1 ||
+                      (enforceLimit && hashesToShow[block.hash] === limit);
+                    if (enforceLimit && hashesToShow[block.hash] > limit) return null;
                     return (
                       <>
                         <HoverableItem
@@ -39,7 +59,11 @@ export const BlocksList: React.FC<FlexProps & { limit?: number; infinite?: boole
                         </HoverableItem>
                         {block.microblocks_accepted.map(
                           (microblockHash, microblockIndex, microblocks) => {
-                            const isLastMicroblock = microblockIndex === microblocks.length - 1;
+                            const isLastMicroblock =
+                              microblockIndex === microblocks.length - 1 ||
+                              (enforceLimit && hashesToShow[microblockHash] === limit);
+                            if (enforceLimit && hashesToShow[microblockHash] > limit) return null;
+
                             return (
                               <SafeSuspense
                                 key={`microblocks-list-${microblockHash}`}
