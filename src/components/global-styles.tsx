6af66ecb0691e387f7933eb4273c06de636cb689
asteria <asteria@syvita.org>
2021-09-05 10:13:46 +0000

    font updates

--- src/components/global-styles.tsx
+++ src/components/global-styles.tsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import { Global, css } from '@emotion/react';
 import { ProgressBarStyles } from '@components/progress-bar';
 import { prismTheme } from '@components/prism-theme';
+import { theme, generateCssVariables } from '@stacks/ui';
 
 const TextAreaOverrides = (
   <Global
     styles={css`
+      @import url('https://x.syvita.org/fonts/roobert/importme.css');
+      @import url('https://x.syvita.org/fonts/inter/importme.css');
       ${prismTheme};
 
       .code-editor {
@@ -50,8 +53,6 @@ const TextAreaOverrides = (
 );
 
 const globalStyles = css`
-  @import url('https://x.syvita.org/fonts/roobert/importme.css');
-  @import url('https://x.syvita.org/fonts/inter/importme.css');
   connect-modal {
     .modal-container {
       z-index: 999999999;
@@ -86,6 +87,56 @@ const globalStyles = css`
   a {
     text-decoration: none;
   }
+
+  * {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+:root {
+  @media (prefers-color-scheme: light) {
+    ${generateCssVariables('light')({ colorMode: 'light', theme })};
+    --colors-highlight-line-bg: rgba(255, 255, 255, 0.1);
+    --colors-accent: #9146ff !important;
+    --colors-brand: #9146ff !important;
+    --colors-text-hover: #9146ff !important;
+  }
+  @media (prefers-color-scheme: dark) {
+    ${generateCssVariables('dark')({ colorMode: 'dark', theme })};
+    --colors-highlight-line-bg: rgba(255, 255, 255, 0.05);
+    --colors-accent: #bfabff !important;
+    --colors-brand: #bfabff !important;
+  }
+}
+
+html,
+body,
+#__next {
+  background: var(--colors-bg);
+  border-color: var(--colors-border);
+
+  @media (prefers-color-scheme: light) {
+    :root {
+      ${generateCssVariables('light')({ colorMode: 'light', theme })};
+      --colors-highlight-line-bg: rgba(255, 255, 255, 0.1);
+    }
+    * {
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      ${generateCssVariables('dark')({ colorMode: 'dark', theme })};
+      --colors-highlight-line-bg: rgba(255, 255, 255, 0.04);
+    }
+    * {
+      -webkit-font-smoothing: subpixel-antialiased;
+      -moz-osx-font-smoothing: auto;
+    }
+  }
+}
 `;
 
 const GlobalStyles = <Global styles={globalStyles} />;
