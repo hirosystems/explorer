f4d71395b8464fd4a6c40fcbd4f725628b0a89a5
asteria <asteria@syvita.org>
2021-09-10 07:53:01 +0000

    styles + analytics

--- src/components/global-styles.tsx
+++ src/components/global-styles.tsx
@@ -89,54 +89,54 @@ const globalStyles = css`
   }
 
   * {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-:root {
-  @media (prefers-color-scheme: light) {
-    ${generateCssVariables('light')({ colorMode: 'light', theme })};
-    --colors-highlight-line-bg: rgba(255, 255, 255, 0.1);
-    --colors-accent: #9146ff !important;
-    --colors-brand: #9146ff !important;
-    --colors-text-hover: #9146ff !important;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
-  @media (prefers-color-scheme: dark) {
-    ${generateCssVariables('dark')({ colorMode: 'dark', theme })};
-    --colors-highlight-line-bg: rgba(255, 255, 255, 0.05);
-    --colors-accent: #bfabff !important;
-    --colors-brand: #bfabff !important;
-  }
-}
 
-html,
-body,
-#__next {
-  background: var(--colors-bg);
-  border-color: var(--colors-border);
-
-  @media (prefers-color-scheme: light) {
-    :root {
+  :root {
+    @media (prefers-color-scheme: light) {
       ${generateCssVariables('light')({ colorMode: 'light', theme })};
       --colors-highlight-line-bg: rgba(255, 255, 255, 0.1);
+      --colors-accent: #9146ff !important;
+      --colors-brand: #9146ff !important;
+      --colors-text-hover: #9146ff !important;
     }
-    * {
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
+    @media (prefers-color-scheme: dark) {
+      ${generateCssVariables('dark')({ colorMode: 'dark', theme })};
+      --colors-highlight-line-bg: rgba(255, 255, 255, 0.05);
+      --colors-accent: #bfabff !important;
+      --colors-brand: #bfabff !important;
     }
   }
 
-  @media (prefers-color-scheme: dark) {
-    :root {
-      ${generateCssVariables('dark')({ colorMode: 'dark', theme })};
-      --colors-highlight-line-bg: rgba(255, 255, 255, 0.04);
+  html,
+  body,
+  #__next {
+    background: var(--colors-bg);
+    border-color: var(--colors-border);
+
+    @media (prefers-color-scheme: light) {
+      :root {
+        ${generateCssVariables('light')({ colorMode: 'light', theme })};
+        --colors-highlight-line-bg: rgba(255, 255, 255, 0.1);
+      }
+      * {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
     }
-    * {
-      -webkit-font-smoothing: subpixel-antialiased;
-      -moz-osx-font-smoothing: auto;
+
+    @media (prefers-color-scheme: dark) {
+      :root {
+        ${generateCssVariables('dark')({ colorMode: 'dark', theme })};
+        --colors-highlight-line-bg: rgba(255, 255, 255, 0.04);
+      }
+      * {
+        -webkit-font-smoothing: subpixel-antialiased;
+        -moz-osx-font-smoothing: auto;
+      }
     }
   }
-}
 `;
 
 const GlobalStyles = <Global styles={globalStyles} />;
