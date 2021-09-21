69c1113324c83b127c9d577cda269233a0f1b492
fbwoolf <fwoolf@blockstack.com>
2021-09-21 16:04:04 -0500

    chore: try nextjs env var syntax

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,10 +4,8 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  // TODO: Add a Sentry Release Github Action when switch to an org auth token
-  // (ex: https://github.com/marketplace/actions/sentry-release)
-  // environment: process.env.NODE_ENV,
-  // release: `${process.env.SENTRY_PROJECT}@${process.env.npm_package_version}`,
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
   tracesSampleRate: 1.0,
 });
