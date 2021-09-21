b82b09bde1dd4a2d277e280ef4b0b168bf0d4a98
fbwoolf <fwoolf@blockstack.com>
2021-09-21 14:28:00 -0500

    chore: add init config options

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -4,6 +4,10 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
+  // TODO: Add a Sentry Release Github Action when switch to an org auth token
+  // (ex: https://github.com/marketplace/actions/sentry-release)
+  // environment: process.env.NODE_ENV,
+  // release: `${process.env.SENTRY_PROJECT}@${process.env.npm_package_version}`,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
 });
