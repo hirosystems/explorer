7ce4a0bc821570cc2d00a069282702545f1f2c3f
asteria <asteria@syvita.org>
2021-12-23 13:59:32 +0000

    change icon

--- src/components/icons/stx-inline.tsx
+++ src/components/icons/stx-inline.tsx
@@ -11,7 +11,7 @@ export const StxInline: ForwardRefExoticComponentWithAs<BoxProps, 'svg'> = forwa
     display="block"
     as={as}
     height="auto"
-    viewBox="0 0 22 22"
+    viewBox="0 0 24 24"
     fill="none"
     ref={ref}
     color={color}
@@ -19,28 +19,8 @@ export const StxInline: ForwardRefExoticComponentWithAs<BoxProps, 'svg'> = forwa
     {...props}
   >
     <path
-      d="M4.18817 0.940674L9.37024 8.99389M9.37024 8.99389H0.266602M9.37024 8.99389H12.7316"
-      stroke="currentColor"
-      strokeWidth={strokeWidth as any}
-      strokeLinejoin="bevel"
-    />
-    <path
-      d="M17.9129 0.940674L12.7308 8.99389H21.8345"
-      stroke="currentColor"
-      strokeWidth={strokeWidth as any}
-      strokeLinejoin="bevel"
-    />
-    <path
-      d="M4.18817 21.4407L9.37024 13.3875M9.37024 13.3875H0.266602M9.37024 13.3875H12.7316"
-      stroke="currentColor"
-      strokeWidth={strokeWidth as any}
-      strokeLinejoin="bevel"
-    />
-    <path
-      d="M17.9129 21.4407L12.7308 13.3875H21.8345"
-      stroke="currentColor"
-      strokeWidth={strokeWidth as any}
-      strokeLinejoin="bevel"
+      d="M16.7364 9.50628C18.7225 9.50628 20.4296 10.2204 21.8577 11.6485C23.2859 13.0544 24 14.7727 24 16.8033C24 18.7894 23.2859 20.4854 21.8577 21.8912C20.4296 23.2971 18.7225 24 16.7364 24L6.32636 24C4.96513 24 3.72664 23.6653 2.61088 22.9958C1.49512 22.304 0.624826 21.378 0 20.2176L7.83264 13.9247C7.81032 13.9247 7.77685 13.9358 7.73222 13.9582C7.68759 13.9805 7.65411 13.9916 7.6318 13.9916C5.71269 13.9916 4.06137 13.311 2.67782 11.9498C1.3166 10.5662 0.635983 8.91492 0.635983 6.99582C0.635983 5.07671 1.3166 3.43654 2.67782 2.07531C4.06137 0.691771 5.71269 0 7.6318 0L17.0042 0C18.1869 0 19.2803 0.27894 20.2845 0.83682C21.311 1.3947 22.159 2.16457 22.8285 3.14644L16.0335 9.60669C16.2343 9.53975 16.4686 9.50628 16.7364 9.50628Z"
+      fill="currentColor"
     />
   </Box>
 ));
