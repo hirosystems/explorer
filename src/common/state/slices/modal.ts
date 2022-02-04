bb28db31cc16ad0c8ae0be7dc794b6bdca51ba90
MrHeidar <heidar.m@live.com>
2022-02-04 12:00:08 -0500

    refactor: remove modal recoil state and introduce redux

---
+++ src/common/state/slices/modal.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { MODALS } from '@common/constants';
+import { useAppSelector } from '@common/state/hooks';
+
+export interface State {
+  openedModal?: MODALS;
+}
+
+const initialState: State = {
+  openedModal: undefined,
+};
+
+export const slice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    openModal: (state, action: PayloadAction<MODALS>) => {
+      state.openedModal = action.payload;
+    },
+    closeModal: state => {
+      state.openedModal = undefined;
+    },
+  },
+});
+
+export const { openModal, closeModal } = slice.actions;
+
+export const getOpenedModal = () => useAppSelector(state => state.modal.openedModal);
+
+export default slice.reducer;
