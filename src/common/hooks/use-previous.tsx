1d9d54511fbf3e060da5f5cfa68e9e89d58235c2
Nicholas Barnett <n-barnett@hotmail.com>
2023-01-25 11:42:41 -0600

    feat(post-conditions): fixing rebase issues

---
+++ src/common/hooks/use-previous.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useRef } from 'react';
+
+function usePrevious<T>(value: T): ReturnType<typeof useRef<T>>['current'] {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+}
+export default usePrevious;
