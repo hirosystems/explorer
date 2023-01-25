841b7f5c891c88d22bddbba1607828c4bcaf75a5
Nicholas Barnett <n-barnett@hotmail.com>
2023-01-25 11:42:41 -0600

    feat(post-conditions): fixing linting issues

--- src/common/hooks/use-previous.tsx
+++ src/common/hooks/use-previous.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 function usePrevious<T>(value: T): ReturnType<typeof useRef<T>>['current'] {
   const ref = useRef<T>();
