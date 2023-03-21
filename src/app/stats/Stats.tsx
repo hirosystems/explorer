0e225c4528afd7cd62361be6b3108252c9c670ab
He1DAr <heidarsaleh@outlook.com>
2023-03-21 12:57:45 -0400

    feat: stats error boundary

--- src/app/stats/Stats.tsx
+++ src/app/stats/Stats.tsx
@@ -8,19 +8,22 @@ import { CurrentStackingCycle } from './CurrentStackingCycle';
 import { LastBlock } from './LastBlock';
 import { NextStackingCycle } from './NextStackingCycle';
 import { StxSupply } from './StxSupply';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export const Stats: FC = () => {
   return (
-    <Card
-      display={'grid'}
-      gridColumnStart={'1'}
-      gridColumnEnd={['2', '2', '3']}
-      gridTemplateColumns={['100%', '100%', '1fr 1fr', '1fr 1fr 1fr 1fr']}
-    >
-      <StxSupply borderRightWidth={['0px', '0px', '1px', '1px']} />
-      <LastBlock borderRightWidth={['0px', '0px', '0px', '1px']} />
-      <CurrentStackingCycle borderRightWidth={['0px', '0px', '1px', '1px']} />
-      <NextStackingCycle />
-    </Card>
+    <ErrorBoundary fallbackRender={() => null}>
+      <Card
+        display={'grid'}
+        gridColumnStart={'1'}
+        gridColumnEnd={['2', '2', '3']}
+        gridTemplateColumns={['100%', '100%', '1fr 1fr', '1fr 1fr 1fr 1fr']}
+      >
+        <StxSupply borderRightWidth={['0px', '0px', '1px', '1px']} />
+        <LastBlock borderRightWidth={['0px', '0px', '0px', '1px']} />
+        <CurrentStackingCycle borderRightWidth={['0px', '0px', '1px', '1px']} />
+        <NextStackingCycle />
+      </Card>
+    </ErrorBoundary>
   );
 };
