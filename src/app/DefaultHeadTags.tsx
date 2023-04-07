822926b73b839e74981b03e31223760666c49d16
Lavanya Kasturi <lavanyakasturi@gmail.com>
2023-04-07 12:12:47 -0500

    Updated URL

--- src/app/DefaultHeadTags.tsx
+++ src/app/DefaultHeadTags.tsx
@@ -31,7 +31,7 @@ export function DefaultHeadTags() {
       <meta name="apple-mobile-web-app-status-bar-style" content="#6287DD" />
 
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="https://explorer.hiro.so" />
+      <meta property="og:site_name" content="explorer.hiro.so" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@stacks" />
       <meta name="twitter:creator" content="@stacks" />
