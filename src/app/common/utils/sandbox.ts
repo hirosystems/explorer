6e6c466a994b1bca23921bd2391ceb058c29ea4f
friedger <mail@friedger.de>
2023-04-20 15:44:55 +0200

    chore: add comment

--- src/app/common/utils/sandbox.ts
+++ src/app/common/utils/sandbox.ts
@@ -1,3 +1,13 @@
+/**
+ * Predicate that a function of a contract should be shown.
+ *
+ * Functions that are private should NOT be shown
+ * Functions that are pox-2.stacks-increase should NOT be shown
+ *
+ * @param contractId the contract id
+ * @param abiFn the abi of the function or
+ * @returns true if the provided function should be shown in the explorer.
+ */
 export function showFn(contractId: string, abiFn: any) {
   return (
     abiFn.access !== 'private' &&
