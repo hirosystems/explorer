5d72f58582c320ef2579022001c66f3a3d3f13a3
He1DAr <heidarsaleh@outlook.com>
2023-04-27 10:33:34 -0400

    feat: stacks network update alert

---
+++ src/ui/ListItem.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import {
+  ListItem as CUIListItem,
+  ListItemProps as CUIListItemProps,
+  forwardRef,
+  useColorMode,
+} from '@chakra-ui/react';
+
+import { UIComponent } from './types';
+
+export type ListItemProps = CUIListItemProps & UIComponent;
+export const ListItem = forwardRef<ListItemProps, 'li'>(({ children, size, ...rest }, ref) => (
+  <CUIListItem
+    ref={ref}
+    width={size || rest.width}
+    height={size || rest.height}
+    minWidth={size || rest.minWidth}
+    minHeight={size || rest.minHeight}
+    borderColor={`border.${useColorMode().colorMode}`}
+    {...rest}
+  >
+    {children}
+  </CUIListItem>
+));
