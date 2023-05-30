2885f4fa144048c387941ff9d2bdc95903f28ead
Shamim Ahmed <theshamimahmed@gmail.com>
2023-05-30 23:31:53 +0530

    fix: handled fetcher function gracefully

--- src/common/api/fetch.ts
+++ src/common/api/fetch.ts
@@ -1,18 +1,22 @@
 import { HIRO_HEADERS, withApiServer } from '@/common/constants';
 
-export const fetcher = (input: RequestInfo, init: RequestInit = {}) => {
+export const fetcher = async (input: RequestInfo, init: RequestInit = {}) => {
   const initHeaders = init.headers || {};
-  return fetch(input, {
-    credentials: 'omit',
-    ...init,
-    headers: { ...initHeaders, ...HIRO_HEADERS },
-  });
+  try {
+    return await fetch(input, {
+      credentials: 'omit',
+      ...init,
+      headers: { ...initHeaders, ...HIRO_HEADERS },
+    });
+  } catch (error) {
+    throw new Error(`Network error: ${error.message}`);
+  }
 };
 
 export const fetchFromApi =
   (apiServer: string) =>
-  async (path: string, opts = {}) => {
-    return fetcher(withApiServer(apiServer)(path), {
-      ...opts,
-    });
-  };
+    async (path: string, opts = {}) => {
+      return fetcher(withApiServer(apiServer)(path), {
+        ...opts,
+      });
+    };
