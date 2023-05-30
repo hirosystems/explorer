d4b15c1b9e483d8b40f1417a864ae62ef6be1a26
Shamim Ahmed <theshamimahmed@gmail.com>
2023-05-30 21:38:43 +0530

    fix: stack progress button fixed

--- src/components/percentage-circle.tsx
+++ src/components/percentage-circle.tsx
@@ -3,24 +3,19 @@ import * as React from 'react';
 
 export const PercentageCircle: React.FC<BoxProps & { percentage: number }> = ({ percentage }) => {
   return (
-    <Box strokeWidth={5} strokeLinecap="round" as="svg" viewBox="0 0 36 36">
+    <Box strokeWidth={2} strokeLinecap="round" as="svg" viewBox="0 0 35 35">
       <Box
         as="path"
         fill="none"
-        stroke={'border'}
+        strokeDasharray={`${percentage * 100}, 100`}
         d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
+      a 15.9155 15.9155 0 0 1 0 31.831
+      a 15.9155 15.9155 0 0 1 0 -31.831"
+        stroke="#444"
       />
-      <Box
-        as="path"
-        fill="none"
-        stroke={'brand'}
-        strokeDasharray={`${percentage}, 100`}
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
+      <text fontSize={'8'} x="10" y="20">
+        {`${Math.round(percentage * 100)}%`}
+      </text>
     </Box>
   );
 };
