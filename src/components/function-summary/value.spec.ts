fe80f8b89d5f380d2a483b4f0e45ae13c074db5d
Shamim Ahmed <theshamimahmed@gmail.com>
2023-06-26 19:07:09 +0530

    fix: lint fix

--- src/components/function-summary/value.spec.ts
+++ src/components/function-summary/value.spec.ts
@@ -1,45 +1,45 @@
-import { getValue } from "./value";
+import { getValue } from './value';
 
 describe('getValue', () => {
-    it('should return the correct value for uint type', () => {
-        const arg = {
-            type: 'uint',
-            repr: 'u123',
-            name: 'some-name',
-        };
-        const btc = null;
-        const expected = '123';
+  it('should return the correct value for uint type', () => {
+    const arg = {
+      type: 'uint',
+      repr: 'u123',
+      name: 'some-name',
+    };
+    const btc = null;
+    const expected = '123';
 
-        const result = getValue(arg, btc);
+    const result = getValue(arg, btc);
 
-        expect(result).toBe(expected);
-    });
+    expect(result).toBe(expected);
+  });
 
-    it('should return the correct value for uint type (with microToStacks conversion)', () => {
-        const arg = {
-            type: 'uint',
-            repr: 'u100000000',
-            name: 'ustx-amount',
-        };
-        const btc = null;
-        const expected = '100.00 STX';
+  it('should return the correct value for uint type (with microToStacks conversion)', () => {
+    const arg = {
+      type: 'uint',
+      repr: 'u100000000',
+      name: 'ustx-amount',
+    };
+    const btc = null;
+    const expected = '100.00 STX';
 
-        const result = getValue(arg, btc);
+    const result = getValue(arg, btc);
 
-        expect(result).toBe(expected);
-    });
+    expect(result).toBe(expected);
+  });
 
-    it('should return the correct value for JSON string', () => {
-        const arg = {
-            type: 'some-type',
-            repr: '{"value": 123}',
-            name: 'some-name',
-        };
-        const btc = null;
-        const expected = 123;
+  it('should return the correct value for JSON string', () => {
+    const arg = {
+      type: 'some-type',
+      repr: '{"value": 123}',
+      name: 'some-name',
+    };
+    const btc = null;
+    const expected = 123;
 
-        const result = getValue(arg, btc);
+    const result = getValue(arg, btc);
 
-        expect(result).toBe(expected);
-    });
+    expect(result).toBe(expected);
+  });
 });
