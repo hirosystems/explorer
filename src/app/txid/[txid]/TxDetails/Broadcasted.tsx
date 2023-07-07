78a6c7e545a3b905e181a931535aaff483d4ee84
dragnoir <mosaixel.org@gmail.com>
2023-07-07 23:12:31 +0100

    feat: show receipt time on transaction page

---
+++ src/app/txid/[txid]/TxDetails/Broadcasted.tsx
@@ -0,0 +1,44 @@
+import { toRelativeTime } from '@/common/utils';
+import { Box, Flex, Icon, Tooltip } from '@/ui/components';
+import * as React from 'react';
+import { FC } from 'react';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+
+import { Transaction, MempoolTransaction } from '@stacks/stacks-blockchain-api-types';
+
+import { KeyValueHorizontal } from '../../../common/components/KeyValueHorizontal';
+import { Value } from '../../../common/components/Value';
+import { isInMempool } from '../utils';
+
+export const Broadcasted: FC<{
+  tx: Transaction | MempoolTransaction;
+}> = ({ tx }) => {
+
+  if (!isInMempool(tx)) return null;
+
+  const ts = tx.receipt_time;
+  if (!ts) return null;
+
+  const readableTs = `${new Date(ts * 1000).toLocaleTimeString()} ${new Date(
+    ts * 1000
+  ).toLocaleDateString()}`;
+
+  return (
+    <KeyValueHorizontal
+      label={'Broadcasted'}
+      value={
+        <>
+          <Box>
+            <Tooltip label={readableTs}>
+              <Flex alignItems="center">
+                <Icon as={AiOutlineClockCircle} size="16px" mr="4px" />
+                <Value>{toRelativeTime(ts * 1000)}</Value>
+              </Flex>
+            </Tooltip>
+          </Box>
+        </>
+      }
+      copyValue={readableTs}
+    />
+  );
+};
