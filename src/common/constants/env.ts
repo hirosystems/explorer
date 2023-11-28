32cf6db958f5b1238fe5f815b1df9024fbdcbc4f
He1DAr <heidarsaleh@outlook.com>
2023-11-28 17:39:14 -0500

    feat: debug env vars

--- src/common/constants/env.ts
+++ src/common/constants/env.ts
@@ -37,3 +37,5 @@ export const HIRO_HEADERS: HeadersInit = {
   'x-hiro-product': 'explorer',
   'x-hiro-version': VERSION,
 };
+
+console.log('process.env', process.env);
--- src/common/utils/analytics.ts
+++ src/common/utils/analytics.ts
@@ -1,9 +1,10 @@
 'use client';
 
 import { AnalyticsBrowser } from '@segment/analytics-next';
-import { SEGMENT_WRITE_KEY } from "../constants/env";
 
-console.log('SEGMENT_WRITE_KEY', SEGMENT_WRITE_KEY)
+import { SEGMENT_WRITE_KEY } from '../constants/env';
+
+console.log('SEGMENT_WRITE_KEY', SEGMENT_WRITE_KEY);
 export const analytics = SEGMENT_WRITE_KEY
   ? AnalyticsBrowser.load({
       writeKey: SEGMENT_WRITE_KEY,
