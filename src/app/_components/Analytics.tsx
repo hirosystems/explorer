4fb973e23222c91a280510729e137998dd6b4d49
He1DAr <heidarsaleh@outlook.com>
2023-11-28 17:08:08 -0500

    feat: test analytics

--- src/app/_components/Analytics.tsx
+++ src/app/_components/Analytics.tsx
@@ -10,6 +10,7 @@ export function Analytics() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
+    console.log('analytics', analytics)
     void analytics?.page();
   }, [pathname, searchParams]);
 
--- src/common/utils/analytics.ts
+++ src/common/utils/analytics.ts
@@ -3,6 +3,7 @@
 import { AnalyticsBrowser } from '@segment/analytics-next';
 import { SEGMENT_WRITE_KEY } from "../constants/env";
 
+console.log('SEGMENT_WRITE_KEY', SEGMENT_WRITE_KEY)
 export const analytics = SEGMENT_WRITE_KEY
   ? AnalyticsBrowser.load({
       writeKey: SEGMENT_WRITE_KEY,
