b04e7920918858b79c331562f76df4409ccbd25e
He1DAr <heidarsaleh@outlook.com>
2023-12-29 21:11:44 -0500

    fix: block list memoization

--- src/app/_components/BlockList/BlockAndMicroblocksItem.tsx
+++ src/app/_components/BlockList/BlockAndMicroblocksItem.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React from 'react';
 
 import { Block } from '@stacks/stacks-blockchain-api-types';
 
@@ -11,45 +11,42 @@ import { Text } from '../../../ui/Text';
 import { BlockListItem } from './BlockListItem';
 import { MicroblockListItem } from './MicroblockListItem';
 
-export const BlockAndMicroblocksItem: React.FC<{ block: Block }> = memo(
-  ({ block }) => {
-    return (
-      <AccordionItem
-        border={'none'}
-        borderBottom={'1px solid var(--stacks-colors-border)'}
-        _last={{ border: 'none' }}
-      >
-        <Flex gap={'6px'}>
-          <BlockListItem block={block} data-test={`block-${block.hash}`} />
-          <AccordionButton
-            flexGrow={0}
-            flexShrink={0}
-            width={'30px'}
-            ml={'auto'}
-            p={0}
-            justifyContent={'center'}
-          >
-            <AccordionIcon />
-          </AccordionButton>
-        </Flex>
-        <AccordionPanel p={'0 30px 0 0'}>
-          {!!block.microblocks_accepted?.length ? (
-            block.microblocks_accepted.map((microblockHash, microblockIndex) => (
-              <MicroblockListItem
-                blockTime={block.burn_block_time}
-                hash={microblockHash}
-                index={microblockIndex}
-                key={microblockHash}
-              />
-            ))
-          ) : (
-            <Text fontSize={'14px'} p={'20px'} align={'center'}>
-              No Microblocks
-            </Text>
-          )}
-        </AccordionPanel>
-      </AccordionItem>
-    );
-  },
-  (prevProps, currentProps) => prevProps.block.hash === currentProps.block.hash
-);
+export const BlockAndMicroblocksItem: React.FC<{ block: Block }> = ({ block }) => {
+  return (
+    <AccordionItem
+      border={'none'}
+      borderBottom={'1px solid var(--stacks-colors-border)'}
+      _last={{ border: 'none' }}
+    >
+      <Flex gap={'6px'}>
+        <BlockListItem block={block} data-test={`block-${block.hash}`} />
+        <AccordionButton
+          flexGrow={0}
+          flexShrink={0}
+          width={'30px'}
+          ml={'auto'}
+          p={0}
+          justifyContent={'center'}
+        >
+          <AccordionIcon />
+        </AccordionButton>
+      </Flex>
+      <AccordionPanel p={'0 30px 0 0'}>
+        {!!block.microblocks_accepted?.length ? (
+          block.microblocks_accepted.map((microblockHash, microblockIndex) => (
+            <MicroblockListItem
+              blockTime={block.burn_block_time}
+              hash={microblockHash}
+              index={microblockIndex}
+              key={microblockHash}
+            />
+          ))
+        ) : (
+          <Text fontSize={'14px'} p={'20px'} align={'center'}>
+            No Microblocks
+          </Text>
+        )}
+      </AccordionPanel>
+    </AccordionItem>
+  );
+};
