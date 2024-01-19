60386af783b5eef8b7eda70fffdcb4d10d9d618e
Nicholas Barnett <n-barnett@hotmail.com>
2024-01-19 15:00:46 -0600

    fix(sip-10): captured error and no longer show unknown error for non sip 10 fts

--- src/common/queries/useFtMetadata.ts
+++ src/common/queries/useFtMetadata.ts
@@ -5,8 +5,10 @@ import {
   useQuery,
   useSuspenseQuery,
 } from '@tanstack/react-query';
+import axios from 'axios';
 
 import { useApi } from '../api/useApi';
+import { useGlobalContext } from '../context/useAppContext';
 
 export function useFtMetadata(
   contractId?: string,
@@ -29,12 +31,47 @@ export function useSuspenseFtMetadata(
   options: any = {}
 ): UseSuspenseQueryResult<FtMetadataResponse> {
   const api = useApi();
+  const basePath = useGlobalContext().activeNetworkKey;
+  console.log({ basePath });
+
+  // Modified query function with try-catch for error handling
+  async function fetchMetadata() {
+    try {
+      // return await api.tokenMetadataApi?.getFtMetadata(contractId);
+      const result = await axios.get(`${basePath}/metadata/v1/ft/${contractId}}`);
+      console.log({ result });
+      return result.data;
+    } catch (error) {
+      console.error('Error fetching metadata:', error);
+      // Handle the error as needed, e.g., return null or a default object
+      return null;
+    }
+  }
+
   return useSuspenseQuery({
     queryKey: ['ft-metadata', contractId],
-    queryFn: () => api.tokenMetadataApi?.getFtMetadata(contractId),
+    queryFn: fetchMetadata,
     retry: false,
     staleTime: Infinity,
     refetchOnWindowFocus: false,
     ...options,
   });
+
+  // const api = useApi();
+  // return useSuspenseQuery({
+  //   queryKey: ['ft-metadata', contractId],
+  //   queryFn: async () => {
+  //     try {
+  //       return await api.tokenMetadataApi?.getFtMetadata(contractId);
+  //     } catch (e) {
+  //       console.log('i have caught the error! in useSuspenseFtMetadata');
+  //       // throw e;
+  //       return null;
+  //     }
+  //   },
+  //   retry: false,
+  //   staleTime: Infinity,
+  //   refetchOnWindowFocus: false,
+  //   ...options,
+  // });
 }
