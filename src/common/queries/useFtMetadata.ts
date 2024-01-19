0fe84abc6aa7d2030fdd7d1e70a82a7e18d729af
Nicholas Barnett <n-barnett@hotmail.com>
2024-01-19 15:03:34 -0600

    fix(sip-10): cleaning pr

--- src/common/queries/useFtMetadata.ts
+++ src/common/queries/useFtMetadata.ts
@@ -5,10 +5,8 @@ import {
   useQuery,
   useSuspenseQuery,
 } from '@tanstack/react-query';
-import axios from 'axios';
 
 import { useApi } from '../api/useApi';
-import { useGlobalContext } from '../context/useAppContext';
 
 export function useFtMetadata(
   contractId?: string,
@@ -31,47 +29,12 @@ export function useSuspenseFtMetadata(
   options: any = {}
 ): UseSuspenseQueryResult<FtMetadataResponse> {
   const api = useApi();
-  const basePath = useGlobalContext().activeNetworkKey;
-  console.log({ basePath });
-
-  // Modified query function with try-catch for error handling
-  async function fetchMetadata() {
-    try {
-      // return await api.tokenMetadataApi?.getFtMetadata(contractId);
-      const result = await axios.get(`${basePath}/metadata/v1/ft/${contractId}}`);
-      console.log({ result });
-      return result.data;
-    } catch (error) {
-      console.error('Error fetching metadata:', error);
-      // Handle the error as needed, e.g., return null or a default object
-      return null;
-    }
-  }
-
   return useSuspenseQuery({
     queryKey: ['ft-metadata', contractId],
-    queryFn: fetchMetadata,
+    queryFn: () => api.tokenMetadataApi?.getFtMetadata(contractId),
     retry: false,
     staleTime: Infinity,
     refetchOnWindowFocus: false,
     ...options,
   });
-
-  // const api = useApi();
-  // return useSuspenseQuery({
-  //   queryKey: ['ft-metadata', contractId],
-  //   queryFn: async () => {
-  //     try {
-  //       return await api.tokenMetadataApi?.getFtMetadata(contractId);
-  //     } catch (e) {
-  //       console.log('i have caught the error! in useSuspenseFtMetadata');
-  //       // throw e;
-  //       return null;
-  //     }
-  //   },
-  //   retry: false,
-  //   staleTime: Infinity,
-  //   refetchOnWindowFocus: false,
-  //   ...options,
-  // });
 }
