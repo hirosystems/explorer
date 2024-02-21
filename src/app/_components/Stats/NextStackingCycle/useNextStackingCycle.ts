5b47510eedac7da135930d1026f6e8687e1a1d9e
Nicholas Barnett <n-barnett@hotmail.com>
2024-02-21 16:13:22 -0600

    feat: all work done except AddressesStacking header card + signers list

--- src/app/_components/Stats/NextStackingCycle/useNextStackingCycle.ts
+++ src/app/_components/Stats/NextStackingCycle/useNextStackingCycle.ts
@@ -3,11 +3,20 @@ import { MICROSTACKS_IN_STACKS } from '../../../../common/utils/utils';
 
 export function useSuspenseNextStackingCycle() {
   const { data: poxData } = useSuspensePoxInforRaw();
-
-  const { next_cycle } = poxData || {};
-  const nextCycleStackedSTX = (next_cycle?.stacked_ustx || 0) / MICROSTACKS_IN_STACKS;
-  const blocksTilNextCyclePreparePhase = next_cycle?.blocks_until_prepare_phase || 0;
-  const blocksTilNextCycleRewardPhase = next_cycle?.blocks_until_reward_phase || 0;
+  const {
+    current_burnchain_block_height: currentBurnchainBlockHeight,
+    next_cycle: {
+      stacked_ustx,
+      blocks_until_prepare_phase,
+      blocks_until_reward_phase,
+      id: next_reward_cycle_id,
+    },
+    reward_cycle_id: currenrRewardCycleId,
+  } = poxData || {};
+  const nextCycleStackedSTX = (stacked_ustx || 0) / MICROSTACKS_IN_STACKS;
+  const blocksTilNextCyclePreparePhase = blocks_until_prepare_phase || 0;
+  const blocksTilNextCycleRewardPhase = blocks_until_reward_phase || 0;
+  const nextCycleBurnBlockHeightStart = currentBurnchainBlockHeight + blocksTilNextCyclePreparePhase;
 
   const numberOfTenMinutesInDay = (24 * 60) / 10;
   const approximateDaysTilNextCyclePreparePhase = Math.floor(
@@ -18,11 +27,14 @@ export function useSuspenseNextStackingCycle() {
   );
   const displayPreparePhaseInfo = approximateDaysTilNextCyclePreparePhase > 0;
   return {
+    currenrRewardCycleId,
+    next_reward_cycle_id,
     nextCycleStackedSTX,
     approximateDaysTilNextCycleRewardPhase,
     displayPreparePhaseInfo,
     blocksTilNextCyclePreparePhase,
     blocksTilNextCycleRewardPhase,
     approximateDaysTilNextCyclePreparePhase,
+    nextCycleBurnBlockHeightStart,
   };
 }
