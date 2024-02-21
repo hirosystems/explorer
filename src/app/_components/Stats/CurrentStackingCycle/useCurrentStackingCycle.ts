da823e0ed14c2f4e4352062b7471adbb621431dc
Nicholas Barnett <n-barnett@hotmail.com>
2024-02-21 16:13:22 -0600

    feat: all work done except AddressesStacking header card + signers list

--- src/app/_components/Stats/CurrentStackingCycle/useCurrentStackingCycle.ts
+++ src/app/_components/Stats/CurrentStackingCycle/useCurrentStackingCycle.ts
@@ -1,26 +1,43 @@
 import { useSuspensePoxInforRaw } from '../../../../common/queries/usePoxInforRaw';
 import { MICROSTACKS_IN_STACKS } from '../../../../common/utils/utils';
 
+const NUM_TEN_MINS_IN_DAY = (24 * 60) / 10;
+
 export function useSuspenseCurrentStackingCycle() {
   const { data: poxData } = useSuspensePoxInforRaw();
+  const {
+    current_cycle: { id: currentCycleId = 0, stacked_ustx = 0 } = {},
+    next_cycle: {
+      blocks_until_reward_phase: blocksTilNextCycleRewardPhase = 0,
+      blocks_until_prepare_phase: blocksTilNextCyclePreparePhase = 0,
+    } = {},
+    next_reward_cycle_in,
+    reward_cycle_length,
+  } = poxData || {};
 
-  const numberOfTenMinutesInDay = (24 * 60) / 10;
-  const { current_cycle, next_reward_cycle_in, next_cycle } = poxData || {};
-  const currentCycleStackedSTX = (current_cycle?.stacked_ustx || 0) / MICROSTACKS_IN_STACKS;
+  const currentCycleStackedSTX = stacked_ustx / MICROSTACKS_IN_STACKS;
   const blocksTilNextCycle = next_reward_cycle_in || 0;
-  const blocksTilNextCyclePreparePhase = next_cycle?.blocks_until_prepare_phase || 0;
-  const blocksTilNextCycleRewardPhase = next_cycle?.blocks_until_reward_phase || 0;
-  const approximateDaysTilNextCycle = Math.floor(blocksTilNextCycle / numberOfTenMinutesInDay);
-  const approximateDaysTilNextCyclePreparePhase = Math.floor(
-    blocksTilNextCyclePreparePhase / numberOfTenMinutesInDay
+  // const blocksTilNextCyclePreparePhase = blocks_until_prepare_phase || 0;
+  // const blocksTilNextCycleRewardPhase = blocks_until_reward_phase || 0;
+  const approximateDaysTilNextCycle = Math.floor(blocksTilNextCycle / NUM_TEN_MINS_IN_DAY);
+  const currentCycleProgressPercentage =
+    (reward_cycle_length - next_reward_cycle_in) / reward_cycle_length;
+  const approximateDaysSinceCurrentCycleStart = Math.floor(
+    (reward_cycle_length - next_reward_cycle_in) / NUM_TEN_MINS_IN_DAY
   );
-  const approximateDaysTilNextCycleRewardPhase = Math.floor(
-    blocksTilNextCycleRewardPhase / numberOfTenMinutesInDay
-  );
-  const displayPreparePhaseInfo = approximateDaysTilNextCyclePreparePhase > 0;
+  // const approximateDaysTilNextCyclePreparePhase = Math.floor(
+  //   blocksTilNextCyclePreparePhase / NUM_TEN_MINS_IN_DAY
+  // );
+  // const approximateDaysTilNextCycleRewardPhase = Math.floor(
+  //   blocksTilNextCycleRewardPhase / NUM_TEN_MINS_IN_DAY
+  // );
+  // const displayPreparePhaseInfo = approximateDaysTilNextCyclePreparePhase > 0;
   return {
+    currentCycleId,
+    currentCycleProgressPercentage,
     currentCycleStackedSTX,
     blocksTilNextCycle,
     approximateDaysTilNextCycle,
+    approximateDaysSinceCurrentCycleStart,
   };
 }
--- src/app/signers/PageClient.tsx
+++ src/app/signers/PageClient.tsx
@@ -1,17 +1,18 @@
 'use client';
 
+import { TokenPrice } from '../../common/types/tokenPrice';
 import { Flex } from '../../ui/Flex';
 import { PageTitle } from '../_components/PageTitle';
 import { SignersHeaderWithErrorBoundary } from './SignersHeader';
 import SignersTable from './SignersTable';
 
-export default function () {
+export default function ({ tokenPrice }: { tokenPrice: TokenPrice }) {
   return (
     <>
       <Flex justifyContent={'space-between'} alignItems={'flex-end'}>
         <PageTitle>Signers</PageTitle>
       </Flex>
-      <SignersHeaderWithErrorBoundary />
+      <SignersHeaderWithErrorBoundary tokenPrice={tokenPrice}/>
       <SignersTable />
     </>
   );
