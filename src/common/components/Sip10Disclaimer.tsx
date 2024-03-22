d3f3d597598e866647d06eed775333a03e892dc2
Nicholas Barnett <n-barnett@hotmail.com>
2024-03-22 13:35:35 -0500

    feat(legal-notification): addressed pr feedback

--- src/common/components/Sip10Disclaimer.tsx
+++ src/common/components/Sip10Disclaimer.tsx
@@ -1,14 +1,15 @@
+import { PiWarningLight } from 'react-icons/pi';
+
 import { Flex } from '../../ui/Flex';
 import { Icon } from '../../ui/Icon';
-import { Text } from '../../ui/Text';
 import { Link } from '../../ui/Link';
-import { PiWarningLight } from 'react-icons/pi';
+import { Text } from '../../ui/Text';
 
 export function Sip10Disclaimer() {
   return (
-    <Flex >
+    <Flex>
       <Icon as={PiWarningLight} color="secondaryText" mr={2} size={4} />
-      <Text color="secondaryText" fontSize=" 14px" lineHeight=" 20px" letterSpacing=" -0.14px">
+      <Text color="secondaryText" fontSize="sm" lineHeight="tall" letterSpacing="normal">
         Please note that SIP-10 data is extracted from third party providers. We make no
         representations or warranties as to the third party content and do not guarantee its
         accuracy, timeliness, completeness or usefulness. For more information, please review our{' '}
