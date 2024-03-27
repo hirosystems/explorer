5eaffe74932242d808feecdad2adf80edf404629
Nicholas Barnett <n-barnett@hotmail.com>
2024-03-27 13:09:49 -0500

    feat(grouped-by-btc-block-list-view-2): finished headers ui

--- src/app/_components/BlockList/GroupedByBurnBlock/BlocksPageHeaders.tsx
+++ src/app/_components/BlockList/GroupedByBurnBlock/BlocksPageHeaders.tsx
@@ -1,24 +1,43 @@
 'use client';
 
+import { ReactNode } from 'react';
+
 import { Card } from '../../../../common/components/Card';
+import { useSuspenseBurnBlocks } from '../../../../common/queries/useBurnBlocks';
+import { Flex } from '../../../../ui/Flex';
+import { Icon } from '../../../../ui/Icon';
 import { Stack } from '../../../../ui/Stack';
 import { Text } from '../../../../ui/Text';
-import { Flex } from '../../../../ui/Flex';
-import { useSuspenseBurnBlocks } from '../../../../common/queries/useBurnBlocks';
+import { BitcoinIcon } from '../../../../ui/icons/BitcoinIcon';
 
 function LastBlockCard() {
-const lastBlock = useSuspenseBurnBlocks(1)
+  const lastBlock = useSuspenseBurnBlocks(1); // TODO: might need to use web socket here
 
   return (
     <Stack padding="22px 38px" gap={3} alignItems="flex-start" flexWrap="nowrap">
       <Text fontSize="xs" fontWeight="medium" whiteSpace="nowrap">
         LAST BLOCK
       </Text>
-      <Text fontSize="xl" fontWeight="medium" whiteSpace="nowrap" display="inline-block" mr={1}>
-        some number
-      </Text>
-      <Text fontSize="xs" fontWeight="medium" color="secondaryText">
-        Some more info
+      <Flex alignItems="center" gap={2}>
+        <Text fontSize="xl" fontWeight="medium" whiteSpace="nowrap" display="inline-block" mr={1}>
+          #124009
+        </Text>
+        <Flex alignItems="center" gap={1}>
+          <Icon as={BitcoinIcon} size={4.5} />
+          <Text
+            fontSize="xl"
+            fontWeight="medium"
+            whiteSpace="nowrap"
+            display="inline-block"
+            mr={1}
+            color="textSubdued"
+          >
+            #889300
+          </Text>
+        </Flex>
+      </Flex>
+      <Text fontSize="xs" fontWeight="medium" color="textSubdued">
+        435 transactions
       </Text>
     </Stack>
   );
@@ -26,29 +45,56 @@ const lastBlock = useSuspenseBurnBlocks(1)
 
 function AverageStacksBlockTimeCard() {
   return (
-    <Flex padding="22px 38px" gap={1} alignItems="flex-start" flexWrap="nowrap">
-      Nothing yet
-    </Flex>
+    <Stack padding="22px 38px" gap={3} alignItems="flex-start" flexWrap="nowrap">
+      <Text fontSize="xs" fontWeight="medium" whiteSpace="nowrap">
+        AVERAGE STACKS BLOCK TIME
+      </Text>
+      <Text fontSize="xl" fontWeight="medium" whiteSpace="nowrap" display="inline-block" mr={1}>
+        48 sec.
+      </Text>
+      <Text fontSize="xs" fontWeight="medium" color="textSubdued">
+        In the last 24hs.
+      </Text>
+    </Stack>
   );
 }
 
-function NextStackingCycleCard() {
+function LastConfirmedBitcoinBlockCard() {
   return (
-    <Flex padding="22px 38px" gap={1} alignItems="flex-start" flexWrap="nowrap">
-      Nothing yet
-    </Flex>
+    <Stack padding="22px 38px" gap={3} alignItems="flex-start" flexWrap="nowrap">
+      <Text fontSize="xs" fontWeight="medium" whiteSpace="nowrap">
+        IN THE LAST CONFIRMED BITCOIN BLOCK
+      </Text>
+      <Flex width="full" display="grid" gridTemplateColumns={['repeat(2, 50%)']}>
+        <Stack gap={2}>
+          <Text fontSize="xl" fontWeight="medium" whiteSpace="nowrap" display="inline-block" mr={1}>
+            214
+          </Text>
+          <Text fontSize="xs" fontWeight="medium" color="textSubdued">
+            Stacks blocks
+          </Text>
+        </Stack>
+        <Stack gap={2}>
+          <Text fontSize="xl" fontWeight="medium" whiteSpace="nowrap" display="inline-block" mr={1}>
+            4354
+          </Text>
+          <Text fontSize="xs" fontWeight="medium" color="textSubdued">
+            Stacks transactions
+          </Text>
+        </Stack>
+      </Flex>
+    </Stack>
   );
 }
 
 export function BlocksPageHeaderLayout({
   lastBlockCard,
   averageStacksBlockTimeCard,
-  nextStackingCycleCard,
+  lastConfirmedBitcoinBlockCard,
 }: {
-  title: ReactNode;
   lastBlockCard: ReactNode;
   averageStacksBlockTimeCard: ReactNode;
-  nextStackingCycleCard: ReactNode;
+  lastConfirmedBitcoinBlockCard: ReactNode;
 }) {
   return (
     <Card
@@ -58,8 +104,8 @@ export function BlocksPageHeaderLayout({
       gridTemplateColumns={['100%', '100%', '100%', 'repeat(3, 33.33%)']}
       sx={{
         '& > *:not(:last-of-type)': {
-          borderBottom: ['1px solid #000', null, null, 'none'], // Apply bottom border on smaller screens
-          borderRight: [null, null, null, '1px solid #000'], // Apply right border on larger screens
+          borderBottom: ['1px solid var(--stacks-colors-borderPrimary)', null, null, 'none'], // Apply bottom border on smaller screens
+          borderRight: [null, null, null, '1px solid var(--stacks-colors-borderPrimary)'], // Apply right border on larger screens
         },
         '& > *:last-of-type': {
           borderBottom: 'none', // Ensure the last item has no bottom border
@@ -69,7 +115,7 @@ export function BlocksPageHeaderLayout({
     >
       {lastBlockCard}
       {averageStacksBlockTimeCard}
-      {nextStackingCycleCard}
+      {lastConfirmedBitcoinBlockCard}
     </Card>
   );
 }
@@ -79,7 +125,7 @@ export function BlocksPageHeaders() {
     <BlocksPageHeaderLayout
       lastBlockCard={<LastBlockCard />}
       averageStacksBlockTimeCard={<AverageStacksBlockTimeCard />}
-      nextStackingCycleCard={<NextStackingCycleCard />}
+      lastConfirmedBitcoinBlockCard={<LastConfirmedBitcoinBlockCard />}
     />
   );
 }
