624885cd990bd01c0f1b0c35bbd48c26ab060878
He1DAr <heidarsaleh@outlook.com>
2024-04-04 11:10:51 -0400

    feat: support page

---
+++ src/app/support/page.tsx
@@ -0,0 +1,11 @@
+import dynamic from 'next/dynamic';
+import * as React from 'react';
+
+import Skeleton from './skeleton';
+
+const Page = dynamic(() => import('./PageClient'), {
+  loading: () => <Skeleton />,
+  ssr: false,
+});
+
+export default Page;
