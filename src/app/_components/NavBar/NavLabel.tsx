f1fb103bdcdef86f8db3a9d46522d84dc1395e1d
Nicholas Barnett <n-barnett@hotmail.com>
2024-04-18 20:16:01 -0500

    feat(update-navbar): cleaning pr

--- src/app/_components/NavBar/NavLabel.tsx
+++ src/app/_components/NavBar/NavLabel.tsx
@@ -1,11 +1,8 @@
 import { ReactNode } from 'react';
 
-import { Box } from '../../../ui/Box';
 import { Flex } from '../../../ui/Flex';
-import { Link } from '../../../ui/Link';
 import { Text } from '../../../ui/Text';
 import { useBreakpointValue } from '../../../ui/hooks/useBreakpointValue';
-import { NavItem } from './types';
 
 const mobileNavLabelStyles = {
   py: 5,
@@ -34,42 +31,3 @@ export function NavLabel({ children, icon }: { children: ReactNode; icon?: React
     </Flex>
   );
 }
-
-export const DesktopSubNav = ({ label, href, onClick }: NavItem) => {
-  return (
-    <Flex
-      alignItems={'center'}
-      borderRadius="xl"
-      _hover={{
-        bg: 'dropdownBgHover',
-      }}
-      width="full"
-    >
-      {href ? (
-        <Link
-          href={href}
-          display="block"
-          rounded="md"
-          color="text"
-          width="fit-content"
-          variant="noUnderline"
-        >
-          {label}
-        </Link>
-      ) : (
-        <Box
-          as={'button'}
-          onClick={onClick}
-          role="group"
-          display="block"
-          rounded="md"
-          color="text"
-          width="full"
-          textAlign={'left'}
-        >
-          {label}
-        </Box>
-      )}
-    </Flex>
-  );
-};
