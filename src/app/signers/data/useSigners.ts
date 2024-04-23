d6a3e13503cbfc0636054be892846851c1d3b362
Nicholas Barnett <n-barnett@hotmail.com>
2024-04-23 18:17:12 -0500

    feat(signers): work in progress

--- src/app/signers/data/useSigners.ts
+++ src/app/signers/data/useSigners.ts
@@ -1,13 +1,22 @@
 import { useGlobalContext } from '@/common/context/useAppContext';
 import { TEN_MINUTES } from '@/common/queries/query-stale-time';
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
-export function useSigners(cycleId: number) {
+export interface SignerInfo {
+  signing_key: string;
+  weight: number;
+  stacked_amount: string;
+  weight_percent: number;
+  stacked_amount_percent: number;
+}
+
+export function useSuspensePoxSigners(cycleId: number) {
   const { url: activeNetworkUrl } = useGlobalContext().activeNetwork;
 
-  return useQuery({
+  return useSuspenseQuery<SignerInfo[]>({
     queryKey: ['signers', cycleId],
-    queryFn: () => fetch(`${activeNetworkUrl}/v2/pox/cycles/${cycleId}/signers`).then(res => res.json()),
+    queryFn: () =>
+      fetch(`${activeNetworkUrl}/v2/pox/cycles/${cycleId}/signers`).then(res => res.json()),
     staleTime: TEN_MINUTES,
   });
 }
