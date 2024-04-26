ac6279c5804b1eade922fdd7ba85c736130f18b8
Nicholas Barnett <n-barnett@hotmail.com>
2024-04-26 14:59:57 -0500

    feat(update-navbar): modified z-indices

--- src/app/_components/StatusBar/StatusBarBase.tsx
+++ src/app/_components/StatusBar/StatusBarBase.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react';
+import { FC, ReactNode } from 'react';
 import { BsExclamationCircle, BsExclamationTriangle } from 'react-icons/bs';
 import { IncidentImpact } from 'statuspage.io';
 
