41986594d0396b529928a175d9570e2b442e2459
Nicholas Barnett <n-barnett@hotmail.com>
2024-05-09 17:41:03 -0500

    feat(signers-page): added mapping

---
+++ src/app/signers/consts.ts
@@ -0,0 +1,66 @@
+export const SIGNER_KEY_MAP = {
+  '03e0df37e83e43847625a0320456cb9758050a61ce76c2c130bf50242f27ba6d54': {
+    poolOperator: 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.stacking-pool-signer-alum-labs-v1',
+    name: 'Alum Labs',
+  },
+  '025588e24e2bf387fe8cc7bccba1aac7fe599b96724892431e992a40d06e8fe220': {
+    poolOperator: 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.stacking-pool-v1',
+    name: 'Stacking DAO',
+  },
+  '023d6ecdc36fa1e1c6a9f116c7f13ae843001ed9d617f66f6c68cabf751bf82555': {
+    poolOperator: 'SP21YTSM60CAY6D011EZVEVNKXVW8FVZE198XEFFP.pox4-fast-pool-v3',
+    name: 'Fast Pool',
+  },
+  '029e1245f007bd8f76d5ce67e759acd21f8b0f2538a80713468f7524bf3fff6136': {
+    poolOperator: 'SP2TXK60BBCSNDE1GNNMHM89KY6ZY4ZHTBTJP8X9V',
+    name: 'Luganodes',
+  },
+  '03cef32afac202346ac76a28e81e77ed497c3f22ce20ac54b496950b4ef0b74b2e': {
+    poolOperator: 'SP1322FH637CQKTNTBM4KR0AXF28P6JCK5V54V5WT',
+    name: 'Staking Defense league',
+  },
+  '024f164c6e73df283d34d7d9cc86553a82dce76045ba7dfbf4de0004f89eabb8e0': {
+    poolOperator: 'SP1VTA9RRQA8FS9KSGGB6PAG89VDSPEYRY74JZASC',
+    name: 'Senseinode',
+  },
+  '026f474e6910a1d5d254ba9ad19fe6a4b9133b7690765257680507a730037929ab': {
+    poolOperator: 'SP2A2HGHMS3EQYRS8E0ACQ64DMS3P6C57A6VBE45V',
+    name: 'Chorus One',
+  },
+  '02254a34747123978819f2a90506f76cb057fe3fbff6d8721a0d9cf8e9412d0e60': {
+    poolOperator: 'SP21D6BW36TSGWAZS8K4JAJVTNXWKQN9G3TH5MG6A',
+    name: 'Blockdaemon',
+  },
+  '034df3feda207a1cd4f31ae2b58f136a0d382d23419ef8d06569fa538202ba8aed': {
+    poolOperator: 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.stacking-pool-signer-chorus-one-v1',
+    name: 'StackingDAO (Chorus one)',
+  },
+  '03cc1da6f6235699284987f7d3a98a950b0c693cdbed87ab33c04f61e2dfb6a177': {
+    poolOperator: 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.stacking-pool-signer-despread-v1',
+    name: 'StackingDAO (Despread)',
+  },
+  '0215245613e31de2ec2e7f2c4facb45724be2e49d9d42abb0a5e571322593b36bf': {
+    poolOperator: 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.stacking-pool-signer-kiln-v1',
+    name: 'StackingDAO (Kiln)',
+  },
+  // '029e1245f007bd8f76d5ce67e759acd21f8b0f2538a80713468f7524bf3fff6136': {
+  //     poolOperator: 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.stacking-pool-signer-luganodes-v1',
+  //     name: 'StackingDAO (Luganodes)'
+  // },
+  '0244869db071d334ff8e5cd94956ae7b60a4abd41f83f3c9d66ab314718151d94d': {
+    poolOperator: 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.stacking-pool-signer-restake-v1',
+    name: 'StackingDAO (Restake)',
+  },
+  '03a541c1ec2cfb32da48cfadf439c9b2f27d166bbffa18a178c7a6a0d54cfa7813': {
+    poolOperator: 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.stacking-pool-signer-blockdaemon-v1',
+    name: 'StackingDAO (Blockdaemon)',
+  },
+  '02877ce29ba35458b827a6ea18510b9058ae4c30e2c33d288f2982c13497caec6e': {
+    poolOperator: 'SPXVRSEH2BKSXAEJ00F1BY562P45D5ERPSKR4Q33',
+    name: 'Xverse',
+  },
+  '0x02b20a0603a409270d4421d89a831e8f7b2fa7c5f2d8872d7aa94737334d10c194': {
+    poolOperator: 'SP3CPP71K21BKT8952BN0FFZQDEDKQ3BMB9M9J8X4',
+    name: 'Luxor',
+  },
+};
