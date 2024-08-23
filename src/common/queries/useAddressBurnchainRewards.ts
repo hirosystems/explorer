3a1262ef3835bf18723d767ccbd6fc01d8be656e
Nicholas Barnett <n-barnett@hotmail.com>
2024-08-23 14:58:31 -0500

    feat: stacking page

---
+++ src/common/queries/useAddressBurnchainRewards.ts
@@ -0,0 +1,26 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { useGlobalContext } from '../context/useGlobalContext';
+
+const ADDRESS_BURNCHAIN_REWARDS_QUERY_KEY = 'address-burnchain-rewards';
+
+// TODO: Update StacksJS to support fetching burnchain rewards
+export interface AddressBurnchainRewards {
+  reward_recipient: 'string';
+  reward_amount: 'string';
+}
+
+export const useSuspenseAddressBurnchainRewards = (poxAddress: string) => {
+  // TODO: check that the address is a valid btc address
+  const { url: activeNetworkUrl } = useGlobalContext().activeNetwork;
+
+  return useSuspenseQuery<AddressBurnchainRewards>({
+    queryKey: [ADDRESS_BURNCHAIN_REWARDS_QUERY_KEY, poxAddress],
+    queryFn: () =>
+      fetch(`${activeNetworkUrl}/extended/v1/burnchain/rewards/${poxAddress}/total`).then(res =>
+        res.json()
+      ),
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+  });
+};
