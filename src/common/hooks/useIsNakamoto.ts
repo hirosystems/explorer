b964c2126154985dbdc3130286c32dc7fe811f45
He1DAr <heidarsaleh@outlook.com>
2024-10-10 09:54:39 -0400

    feat: nakamoto 3.0 primary testnet updates

--- src/common/hooks/useIsNakamoto.ts
+++ src/common/hooks/useIsNakamoto.ts
@@ -1,10 +1,9 @@
 import { useGlobalContext } from '../context/useGlobalContext';
 
 export function useRenderNewBlockList() {
-  const { activeNetworkKey, activeNetwork } = useGlobalContext();
+  const { activeNetworkKey } = useGlobalContext();
 
   const isMainnet = activeNetworkKey === 'https://api.hiro.so';
-  const isTestnet = activeNetworkKey === 'https://api.testnet.hiro.so';
 
-  return !isMainnet && !isTestnet;
+  return !isMainnet;
 }
