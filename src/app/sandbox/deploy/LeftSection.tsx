5713ba0b16ec408bcd3dcf3f0a6293fac1c583ad
Nicholas Barnett <n-barnett@hotmail.com>
2024-10-22 15:59:32 -0500

    feat: added deploy note text about clarinet

--- src/app/sandbox/deploy/LeftSection.tsx
+++ src/app/sandbox/deploy/LeftSection.tsx
@@ -13,12 +13,14 @@ import { useAppDispatch, useAppSelector } from '../../../common/state/hooks';
 import { Box } from '../../../ui/Box';
 import { Button } from '../../../ui/Button';
 import { Flex } from '../../../ui/Flex';
-import { HStack } from '../../../ui/HStack';
+import { Icon } from '../../../ui/Icon';
 import { IconButton } from '../../../ui/IconButton';
 import { Input } from '../../../ui/Input';
 import { InputGroup } from '../../../ui/InputGroup';
 import { InputRightElement } from '../../../ui/InputRightElement';
 import { Stack } from '../../../ui/Stack';
+import { Text } from '../../../ui/Text';
+import { TextLink } from '../../../ui/TextLink';
 import { Tooltip } from '../../../ui/Tooltip';
 import { Caption, Title } from '../../../ui/typography';
 import { useUser } from '../hooks/useUser';
@@ -51,67 +53,95 @@ export function LeftSection() {
         Write & Deploy
       </Title>
       <Caption mb="8px">Contract name</Caption>
-      <Flex>
-        <InputGroup
-          border="1px solid transparent"
-          alignItems="center"
-          borderRadius="24px"
-          position="relative"
-        >
-          <Input
-            value={contractName}
-            onChange={(e: any) => setContractName(e.target?.value as string)}
-            placeholder="Name your contract"
-            pr="84px"
-          />
-          <InputRightElement>
-            <IconButton
-              onClick={() => {
-                setContractName('');
-              }}
-              icon={<X size={4} />}
-              aria-label={'clear contract name field'}
-            />
-          </InputRightElement>
-        </InputGroup>
-        <Box onClick={() => dispatch(toggleCodeToolbar())}>
-          <Tooltip label="Contract tools">
-            <IconButton icon={<Toolbox size={4} />} aria-label={'contract tools'} size={'40px'} />
-          </Tooltip>
-        </Box>
-      </Flex>
-      <HStack alignItems="center" justifyContent={'center'} gap={2} mt="20px">
-        <Button
-          onClick={() =>
-            isConnected
-              ? onDeploy()
-              : connect({
-                  onFinish: authData => {
-                    dispatch(setUserData({ userData: authData.userSession.loadUserData() }));
-                  },
-                })
-          }
-          width="100%"
-          variant={'primary'}
-        >
-          {isConnected ? 'Deploy' : 'Connect Stacks Wallet'}
-        </Button>
-      </HStack>
-      <Stack mt={'10px'}>
-        <Button
-          variant="secondary"
-          rightIcon={<ArrowSquareOut />}
-          onClick={() => {
-            const paramsBase64 = { name: contractName, sourceCode: codeBody };
-            const state = btoa(JSON.stringify(paramsBase64));
+      <Stack gap={3}>
+        <Stack gap={5}>
+          <Flex>
+            <InputGroup
+              border="1px solid transparent"
+              alignItems="center"
+              borderRadius="24px"
+              position="relative"
+            >
+              <Input
+                value={contractName}
+                onChange={(e: any) => setContractName(e.target?.value as string)}
+                placeholder="Name your contract"
+                pr="84px"
+              />
+              <InputRightElement>
+                <IconButton
+                  onClick={() => {
+                    setContractName('');
+                  }}
+                  icon={<Icon as={X} size={4} />}
+                  aria-label={'clear contract name field'}
+                />
+              </InputRightElement>
+            </InputGroup>
+            <Box onClick={() => dispatch(toggleCodeToolbar())}>
+              <Tooltip label="Contract tools">
+                <IconButton
+                  icon={<Icon as={Toolbox} size={4} />}
+                  aria-label={'contract tools'}
+                  size={'40px'}
+                />
+              </Tooltip>
+            </Box>
+          </Flex>
+          <Stack alignItems="center" justifyContent={'center'} gap={2}>
+            <Button
+              onClick={() =>
+                isConnected
+                  ? onDeploy()
+                  : connect({
+                      onFinish: authData => {
+                        dispatch(setUserData({ userData: authData.userSession.loadUserData() }));
+                      },
+                    })
+              }
+              width="100%"
+              variant={'primary'}
+            >
+              {isConnected ? 'Deploy' : 'Connect Stacks Wallet'}
+            </Button>
+          </Stack>
+        </Stack>
+        <Stack gap={2}>
+          <Button
+            variant="secondary"
+            rightIcon={<ArrowSquareOut />}
+            onClick={() => {
+              const paramsBase64 = { name: contractName, sourceCode: codeBody };
+              const state = btoa(JSON.stringify(paramsBase64));
 
-            // 'state' param is used by Auth0 to forward the params after the login portal
-            window.open(`https://platform.hiro.so/projects/import?state=${state}`);
-          }}
-          width="100%"
-        >
-          Save Contract in Hiro Platform
-        </Button>
+              // 'state' param is used by Auth0 to forward the params after the login portal
+              window.open(`https://platform.hiro.so/projects/import?state=${state}`);
+            }}
+            width="100%"
+          >
+            Save Contract in Hiro Platform
+          </Button>
+          <Text fontSize="xs" as="p">
+            The sandbox doesn't support Clarity 3 deployments. To deploy a Clarity 3 contract,
+            please use{' '}
+            <TextLink
+              href="https://docs.hiro.so/stacks/clarinet/guides/deploy-a-contract"
+              target="_blank"
+              _hover={{ textDecoration: 'underline' }}
+            >
+              Clarinet
+            </TextLink>{' '}
+            or the{' '}
+            <TextLink
+              href="https://docs.hiro.so/stacks/platform/guides/deployment-plans"
+              target="_blank"
+              _hover={{ textDecoration: 'underline' }}
+            >
+              Hiro platform
+            </TextLink>{' '}
+            instead.
+          </Text>
+        </Stack>
       </Stack>
     </>
   );
