7db8422d4bc4a01f9c7de4bd5747035a927735e6
He1DAr <heidarsaleh@outlook.com>
2024-11-07 10:54:45 -0500

    feat: explocot disable dangerouslyallowsvg

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const { withSentryConfig } = require('@sentry/nextjs');
 const nextConfig = {
   output: 'standalone',
   images: {
+    dangerouslyAllowSVG: false,
     remotePatterns: [
       {
         protocol: 'https',
