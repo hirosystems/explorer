c2f664a0f99e0b3a91b644729b15722b1a1d5880
Nicholas Barnett <n-barnett@hotmail.com>
2024-12-17 10:59:31 -0600

    feat: added confirmed and pending deposits

---
+++ src/app/token/[tokenId]/consts.ts
@@ -0,0 +1,2 @@
+export const sbtcDepositAddress = 'SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4';
+export const sbtcContractAddress = 'SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.sbtc-token';
--- src/app/tokens/utils.ts
+++ src/app/tokens/utils.ts
@@ -1,3 +1,5 @@
+import { sbtcContractAddress } from '../token/[tokenId]/consts';
+
 export const getHasSBTCInName = (name: string, symbol: string) => {
   if (!name || !symbol) {
     return false;
@@ -8,5 +10,5 @@ export const getIsSBTC = (contractPrincipal: string) => {
   if (!contractPrincipal) {
     return false;
   }
-  return contractPrincipal === 'SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.sbtc-token';
+  return contractPrincipal === sbtcContractAddress;
 };
