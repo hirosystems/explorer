de94b084b9aab33b23841350c4c24a1837e0babe
Nicholas Barnett <n-barnett@hotmail.com>
2024-12-17 17:30:46 -0600

    fix: overriding marketcap

--- src/app/token/[tokenId]/TokenInfo/MarketCap.tsx
+++ src/app/token/[tokenId]/TokenInfo/MarketCap.tsx
@@ -11,16 +11,29 @@ export const MarketCap: FC<
     marketCap: number | null | undefined;
     tradingVolume24h: number | null | undefined;
     tradingVolumeChangePercentage24h: number | null | undefined;
+    marketCapOverride?: number | null | undefined;
   }
-> = ({ marketCap, tradingVolumeChangePercentage24h, tradingVolume24h, ...gridProps }) => {
+> = ({
+  marketCap,
+  tradingVolumeChangePercentage24h,
+  tradingVolume24h,
+  marketCapOverride,
+  ...gridProps
+}) => {
   return (
     <StatSection
       title="Market Cap"
-      bodyMainText={marketCap ? `$${abbreviateNumber(marketCap)}` : 'N/A'}
+      bodyMainText={
+        marketCapOverride
+          ? `$${abbreviateNumber(marketCapOverride, 2)}`
+          : marketCap
+            ? `$${abbreviateNumber(marketCap, 2)}`
+            : 'N/A'
+      }
       bodySecondaryText={null}
       caption={
         <Flex fontSize={'12px'} fontWeight="500" alignItems={'center'} gap={'6px'}>
-          Trading Volume: ${tradingVolume24h ? abbreviateNumber(tradingVolume24h) : 'N/A'}
+          Trading Volume: ${tradingVolume24h ? abbreviateNumber(tradingVolume24h, 2) : 'N/A'}
           {tradingVolumeChangePercentage24h ? (
             <TrendArrow change={tradingVolumeChangePercentage24h} size={'11px'} />
           ) : null}
