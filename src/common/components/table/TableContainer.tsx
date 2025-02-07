27dddc7c042c1d97a320fdf8f1c53c6f3b90f461
Nicholas Barnett <n-barnett@hotmail.com>
2025-02-07 16:15:13 -0600

    feat: added table pagination

--- src/common/components/table/TableContainer.tsx
+++ src/common/components/table/TableContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, FlexProps } from '@chakra-ui/react';
+import { Box, FlexProps, Stack } from '@chakra-ui/react';
 
 import { Card } from '../Card';
 
@@ -6,19 +6,27 @@ interface TableContainerProps extends FlexProps {}
 
 export function TableContainer({ children, ...rest }: TableContainerProps) {
   return (
-    <Card
-      h="fit-content"
-      w="full"
-      px={3}
-      pb={3}
-      pt={0}
-      borderColor="redesignBorderSecondary"
-      className="table-container-card"
-      {...rest}
-    >
-      <Box overflowX="auto" overflowY="hidden" h="full" w="full" className="table-scroll-container">
-        {children}
-      </Box>
-    </Card>
+    <Stack gap={0} alignItems="center" w="full">
+      <Card
+        h="fit-content"
+        w="full"
+        px={3}
+        pb={3}
+        pt={0}
+        borderColor="redesignBorderSecondary"
+        className="table-container-card"
+        {...rest}
+      >
+        <Box
+          overflowX="auto"
+          overflowY="hidden"
+          h="full"
+          w="full"
+          className="table-scroll-container"
+        >
+          {children}
+        </Box>
+      </Card>
+    </Stack>
   );
 }
