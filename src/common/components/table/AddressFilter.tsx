daa75bacd39b2f6172df1057cacfe8483fed70e2
Nicholas Barnett <n-barnett@hotmail.com>
2025-03-14 14:34:00 -0500

    feat: wp

--- src/common/components/table/AddressFilter.tsx
+++ src/common/components/table/AddressFilter.tsx
@@ -1,16 +1,17 @@
-import { Box, Flex, Icon, Stack } from '@chakra-ui/react';
-import { ArrowDownRight, ArrowUpRight, CaretDown } from '@phosphor-icons/react';
+import { Box, PopoverRootProps, Stack } from '@chakra-ui/react';
 import { Field, FieldProps, Form, Formik } from 'formik';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
-import { truncateMiddleDeprecated } from '../../../common/utils/utils';
 import { Field as ChakraField } from '../../../components/ui/field';
-import { PopoverRoot } from '../../../components/ui/popover';
 import { Button } from '../../../ui/Button';
 import { ExpandingTextarea } from '../../../ui/ExpandingTextarea';
 import { Text } from '../../../ui/Text';
-import { GooseNeckPopoverTrigger, GooseNeckPopoverContent } from '../GooseNeckPopover';
+import {
+  GooseNeckPopoverContent,
+  GooseNeckPopoverRoot,
+  GooseNeckPopoverTrigger,
+} from '../GooseNeckPopover';
 
 interface AddressFilterProps {
   defaultFromAddress?: string;
@@ -35,65 +36,26 @@ export function AddressFilter({
 
   const searchParams = useSearchParams();
   const router = useRouter();
+
   return (
-    <PopoverRoot
+    <GooseNeckPopoverRoot
       id={'address-filter-popover'}
       positioning={{ placement: 'bottom-start' }}
       open={open}
-      onOpenChange={e => setOpen(e.open)}
+      onOpenChange={e => {
+        setOpen(e.open);
+      }}
     >
-      <GooseNeckPopoverTrigger asChild open={open} gooseNeckHeight={9} gooseNeckAdjustment={0}>
-        <Button
-          variant="secondary"
-          fontSize={'sm'}
-          rightIcon={
-            <Icon h={3.5} w={3.5} style={{ strokeWidth: '2px' }}>
-              <CaretDown />
-            </Icon>
-          }
-          height={9}
-        >
-          <Flex gap={0.5}>
-            {!!defaultToAddress || !!defaultFromAddress ? (
-              <Text display={['none', 'none', 'inline']}>
-                Sent/Received{defaultToAddress || defaultFromAddress ? ':' : ''}
-              </Text>
-            ) : (
-              <Text>Sent/Received</Text>
-            )}
-            {!!defaultFromAddress && (
-              <Flex alignItems={'center'} color={'text'}>
-                <Icon h={3} w={3}>
-                  <ArrowDownRight />
-                </Icon>
-                <Text>
-                  {defaultFromAddress.length > 10
-                    ? truncateMiddleDeprecated(defaultFromAddress, 3)
-                    : defaultFromAddress}
-                </Text>
-              </Flex>
-            )}
-            {!!defaultFromAddress && !!defaultToAddress && (
-              <Text color={'text'} fontSize={'sm'}>
-                -
-              </Text>
-            )}
-            {!!defaultToAddress && (
-              <Flex alignItems={'center'} color={'text'}>
-                <Icon h={3} w={3}>
-                  <ArrowUpRight />
-                </Icon>
-                <Text>
-                  {defaultToAddress.length > 10
-                    ? truncateMiddleDeprecated(defaultToAddress, 3)
-                    : defaultToAddress}
-                </Text>
-              </Flex>
-            )}
-          </Flex>
-        </Button>
+      <GooseNeckPopoverTrigger
+        asChild
+        open={open}
+        gooseNeckHeight={9}
+        gooseNeckAdjustment={0}
+        placement="bottom-start"
+      >
+        <Text textStyle="text-medium-sm">From/To</Text>
       </GooseNeckPopoverTrigger>
-      <GooseNeckPopoverContent maxWidth={'275px'} bgColor={'surface'}>
+      <GooseNeckPopoverContent maxW={'275px'} bgColor={'surfacePrimary'} placement="bottom-start">
         <Stack gap={2} p={4}>
           <Formik
             enableReinitialize
@@ -124,7 +86,11 @@ export function AddressFilter({
                       <ChakraField
                         invalid={!!form.errors.fromAddress}
                         errorText={form.errors.fromAddress}
-                        label="From:"
+                        label={
+                          <Text textStyle="text-medium-xs" color="textSecondary">
+                            From:
+                          </Text>
+                        }
                       >
                         <ExpandingTextarea
                           {...field}
@@ -144,7 +110,11 @@ export function AddressFilter({
                       <ChakraField
                         invalid={!!form.errors.toAddress}
                         errorText={form.errors.toAddress}
-                        label="To:"
+                        label={
+                          <Text textStyle="text-medium-xs" color="textSecondary">
+                            To:
+                          </Text>
+                        }
                       >
                         <ExpandingTextarea
                           {...field}
@@ -163,12 +133,10 @@ export function AddressFilter({
                 <Box mt={4}>
                   <Button
                     isLoading={isValidating}
-                    width="100%"
+                    width="full"
                     type="submit"
-                    fontSize={'sm'}
-                    variant={'secondary'}
-                    height={10}
-                    color="textSubdued"
+                    size={'small'}
+                    variant={'redesignSecondary'}
                   >
                     Apply
                   </Button>
@@ -178,6 +146,6 @@ export function AddressFilter({
           </Formik>
         </Stack>
       </GooseNeckPopoverContent>
-    </PopoverRoot>
+    </GooseNeckPopoverRoot>
   );
 }
