ab60caf86f63cc8cf8fecccedc8be1a6d65d1861
Nicholas Barnett <n-barnett@hotmail.com>
2025-03-19 16:50:22 -0500

    feat: added tx page overview section

--- src/app/transactions/PageClient.tsx
+++ src/app/transactions/PageClient.tsx
@@ -4,12 +4,14 @@ import { TxTableFilters } from '@/common/components/table/TxTableFilters';
 import { TxsTable } from '@/common/components/table/table-examples/TxsTable';
 import { isRedesignUrl } from '@/common/utils/url-utils';
 import { TxListTabs } from '@/features/txs-list/tabs/TxListTabs';
-import { ClientOnly, Flex } from '@chakra-ui/react';
+import { Text } from '@/ui/Text';
+import { ClientOnly, Flex, Stack } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
 
 import { TokenPrice } from '../../common/types/tokenPrice';
 import { PageTitle } from '../_components/PageTitle';
 import { FilterProps } from '../search/filters';
+import { Overview } from './Overview';
 import { MempoolFeeStatsSkeleton } from './skeleton';
 
 const MempoolFeeStatsDynamic = dynamic(
@@ -32,10 +34,18 @@ export default function ({ tokenPrice, filters }: { tokenPrice: TokenPrice } & F
       <TxListTabs filters={filters} />
       <ClientOnly>
         {isRedesign ? (
-          <>
-            <TxTableFilters filters={filters} />
-            <TxsTable filters={filters} />
-          </>
+          <Stack gap={24}>
+            <Overview />
+            <Stack gap={8}>
+              <Text fontSize="3.5xl" color="textPrimary">
+                Latest transactions
+              </Text>
+              <Stack gap={5}>
+                <TxTableFilters filters={filters} />
+                <TxsTable filters={filters} />
+              </Stack>
+            </Stack>
+          </Stack>
         ) : null}
       </ClientOnly>
     </>
