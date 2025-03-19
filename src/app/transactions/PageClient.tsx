213ee7c0a9a5e3a4cc8d5e70976909ab797037fa
Nicholas Barnett <n-barnett@hotmail.com>
2025-03-19 13:24:40 -0500

    feat: wp

--- src/app/transactions/PageClient.tsx
+++ src/app/transactions/PageClient.tsx
@@ -4,9 +4,11 @@ import { TxTableFilters } from '@/common/components/table/TxTableFilters';
 import { TxsTable } from '@/common/components/table/table-examples/TxsTable';
 import { isRedesignUrl } from '@/common/utils/url-utils';
 import { TxListTabs } from '@/features/txs-list/tabs/TxListTabs';
+import { ClientOnly, Flex } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
 
 import { TokenPrice } from '../../common/types/tokenPrice';
+import { PageTitle } from '../_components/PageTitle';
 import { FilterProps } from '../search/filters';
 import { MempoolFeeStatsSkeleton } from './skeleton';
 
@@ -21,17 +23,21 @@ const MempoolFeeStatsDynamic = dynamic(
 export default function ({ tokenPrice, filters }: { tokenPrice: TokenPrice } & FilterProps) {
   const isRedesign = isRedesignUrl();
 
-  // TODO: set filters in redux
   return (
     <>
-      {/* <Flex justifyContent={'space-between'} alignItems={'flex-end'}>
+      <Flex justifyContent={'space-between'} alignItems={'flex-end'}>
         <PageTitle>Transactions</PageTitle>
-      </Flex> */}
-      {/* <MempoolFeeStatsDynamic tokenPrice={tokenPrice} /> */}
-      {/* <TxListTabs filters={filters} /> */}
-      <TxTableFilters filters={filters} />
-      <TxsTable filters={filters} />
-      {/* <ClientOnly>{isRedesign ? <TxsTable /> : null}</ClientOnly> */}
+      </Flex>
+      <MempoolFeeStatsDynamic tokenPrice={tokenPrice} />
+      <TxListTabs filters={filters} />
+      <ClientOnly>
+        {isRedesign ? (
+          <>
+            <TxTableFilters filters={filters} />
+            <TxsTable filters={filters} />
+          </>
+        ) : null}
+      </ClientOnly>
     </>
   );
 }
