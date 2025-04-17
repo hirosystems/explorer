848ee0920d7cdfe7b16dbe38e72c0e3903fb39c8
Nicholas Barnett <n-barnett@hotmail.com>
2025-04-17 14:16:10 -0500

    fix: wp

--- src/app/_components/PageWrapper.tsx
+++ src/app/_components/PageWrapper.tsx
@@ -102,7 +102,7 @@ export function PageWrapper({
       <WrapperWithBg serverThemeCookie={serverThemeCookie}>
         <Stack mx="auto" width="full" maxWidth="breakpoint-xl" p={6} minHeight={'100vh'}>
           {/* {isRedesign ? <NewNavBar /> : <NavBar tokenPrice={tokenPrice} />} */}
-          <NewNavBar />
+          <NewNavBar tokenPrices={tokenPrice} />
           <Stack
             // marginTop={isRedesign ? '50px' : '120px'}
             marginTop={'50px'}
