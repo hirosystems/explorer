edd91f36f898aa374a81b17f668cec08f405a75c
Nicholas Barnett <n-barnett@hotmail.com>
2025-04-30 12:26:14 -0500

    feat: wp

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -21,73 +21,65 @@ export interface CommonSearchParams {
   api?: string;
 }
 
-export interface RedesignSearchParams {
-  redesign?: string;
-}
-
 export interface TxPageSearchParams
   extends TxPageFilters,
-    CommonSearchParams,
-    RedesignSearchParams {}
+    CommonSearchParams {}
 
 export default async function (props: { searchParams: Promise<TxPageSearchParams> }) {
   const searchParams = await props.searchParams;
-  const { startTime, endTime, chain, api, fromAddress, toAddress, redesign } = searchParams;
+  const { startTime, endTime, chain, api, fromAddress, toAddress } = searchParams;
 
   const apiUrl = getApiUrl(chain || NetworkModes.Mainnet, api);
 
   let initialTxTableData: GenericResponseType<CompressedTxTableData> | undefined;
 
-  if (redesign === 'true') {
-    try {
-      let bnsAddress;
-      if (apiUrl && fromAddress?.endsWith('.btc')) {
-        bnsAddress = await fetchBnsAddress(apiUrl, fromAddress);
-      }
-      if (apiUrl && toAddress?.endsWith('.btc')) {
-        bnsAddress = await fetchBnsAddress(apiUrl, toAddress);
-      }
-
-      const params = new URLSearchParams({
-        limit: `${TX_TABLE_PAGE_SIZE}`,
-        offset: '0',
-        ...(startTime && { start_time: startTime }),
-        ...(endTime && { end_time: endTime }),
-        ...(fromAddress && { from_address: bnsAddress || fromAddress }),
-        ...(toAddress && { to_address: bnsAddress || toAddress }),
-      });
-      const fetchUrl = `${apiUrl}/extended/v1/tx/?${params.toString()}`;
-      const response = await fetch(fetchUrl, {
-        next: {
-          revalidate: 20, // nextjs caches the response for 20s (about 2-3 blocks)
-        },
-      });
-
-      const data = await response.json();
-      const compressedData = {
-        ...data,
-        results: compressTransactions(data.results),
-      };
-
-      initialTxTableData = compressedData;
-    } catch (error) {
-      logError(
-        error as Error,
-        'Transaction page server-side fetch for initialTxTableData',
-        {
-          fromAddress,
-          toAddress,
-          startTime,
-          endTime,
-          api,
-          chain,
-          redesign,
-          apiUrl,
-          initialTxTableData,
-        },
-        'error'
-      );
+  try {
+    let bnsAddress;
+    if (apiUrl && fromAddress?.endsWith('.btc')) {
+      bnsAddress = await fetchBnsAddress(apiUrl, fromAddress);
     }
+    if (apiUrl && toAddress?.endsWith('.btc')) {
+      bnsAddress = await fetchBnsAddress(apiUrl, toAddress);
+    }
+
+    const params = new URLSearchParams({
+      limit: `${TX_TABLE_PAGE_SIZE}`,
+      offset: '0',
+      ...(startTime && { start_time: startTime }),
+      ...(endTime && { end_time: endTime }),
+      ...(fromAddress && { from_address: bnsAddress || fromAddress }),
+      ...(toAddress && { to_address: bnsAddress || toAddress }),
+    });
+    const fetchUrl = `${apiUrl}/extended/v1/tx/?${params.toString()}`;
+    const response = await fetch(fetchUrl, {
+      next: {
+        revalidate: 20, // nextjs caches the response for 20s (about 2-3 blocks)
+      },
+    });
+
+    const data = await response.json();
+    const compressedData = {
+      ...data,
+      results: compressTransactions(data.results),
+    };
+
+    initialTxTableData = compressedData;
+  } catch (error) {
+    logError(
+      error as Error,
+      'Transaction page server-side fetch for initialTxTableData',
+      {
+        api,
+        chain,
+        apiUrl,
+        fromAddress,
+        toAddress,
+        startTime,
+        endTime,
+        initialTxTableData,
+      },
+      'error'
+    );
   }
 
   return (
