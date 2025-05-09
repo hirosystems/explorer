899639e5865bb144b002d6b1392ba6653c83bef4
Nicholas Barnett <n-barnett@hotmail.com>
2025-05-09 18:13:43 -0500

    fix: fixed time utils test

--- src/common/utils/__tests__/time-utils.test.ts
+++ src/common/utils/__tests__/time-utils.test.ts
@@ -12,6 +12,21 @@ describe('formatTimestampTo12HourTime', () => {
 
   it('formats timestamp as 12-hour time in local time', () => {
     const timestamp = 1718101530;
+    const originalDate = global.Date;
+    
+    // Mock Date to return consistent values
+    global.Date = class extends Date {
+      constructor() {
+        super();
+      }
+      getHours() { return 5; }
+      getMinutes() { return 25; }
+      getSeconds() { return 30; }
+    } as any;
+
     expect(formatTimestampTo12HourTime(timestamp, { useLocalTime: true })).toBe('5:25:30 AM');
+
+    // Restore original Date
+    global.Date = originalDate;
   });
 });
