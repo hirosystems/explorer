4b548e27e5d2dbfe97d0a5fbf5388fbddafd81a9
Nicholas Barnett <n-barnett@hotmail.com>
2025-05-09 18:13:43 -0500

    fix: fixed time utils test

--- src/app/_components/RecentBlocks/__tests__/formatTimestamp.test.ts
+++
@@ -1,9 +0,0 @@
-import { formatTimestamp } from '../utils';
-
-describe('formatTimestamp', () => {
-  it('should format a timestamp correctly', () => {
-    const timestamp = 1672531199000;
-    const formatted = formatTimestamp(timestamp);
-    expect(formatted).toBe('11:43:20 PM (UTC)');
-  });
-});
