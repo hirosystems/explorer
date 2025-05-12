308f120f580bde3adab56aeff35011127228d912
Nicholas Barnett <n-barnett@hotmail.com>
2025-05-12 14:14:32 -0500

    feat: added banner

--- src/app/_components/StatusBar/__tests__/StatusBarBase.test.tsx
+++ src/app/_components/StatusBar/__tests__/StatusBarBase.test.tsx
@@ -2,8 +2,6 @@ import { system } from '@/ui/theme/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import React from 'react';
-import { IncidentImpact } from 'statuspage.io';
 
 import { StatusBarBase } from '../StatusBarBase';
 
@@ -11,7 +9,7 @@ describe('StatusBarBase', () => {
   it('renders correctly (critical impact)', () => {
     const { asFragment } = render(
       <ChakraProvider value={system}>
-        <StatusBarBase impact={IncidentImpact.Critical} content={<div>Test Content</div>} />
+        <StatusBarBase content={<div>Test Content</div>} />
       </ChakraProvider>
     );
     expect(asFragment()).toMatchSnapshot();
@@ -20,7 +18,7 @@ describe('StatusBarBase', () => {
   it('renders correctly (non-critical impact)', () => {
     const { asFragment } = render(
       <ChakraProvider value={system}>
-        <StatusBarBase impact={IncidentImpact.Minor} content={<div>Test Content</div>} />
+        <StatusBarBase content={<div>Test Content</div>} />
       </ChakraProvider>
     );
     expect(asFragment()).toMatchSnapshot();
