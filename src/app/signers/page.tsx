c0791894103b5c0b4d9dc53754e3e6f3c7a37c63
Alex H <alexander.t.huth@gmail.com>
2025-05-19 13:19:06 -0700

    feat: remove stacking section from signers page

--- src/app/signers/page.tsx
+++ src/app/signers/page.tsx
@@ -1,13 +1,11 @@
 import dynamic from 'next/dynamic';
 
-import { getTokenPrice } from '../getTokenPriceInfo';
 import { SignersPageSkeleton } from './skeleton';
 
 const Page = dynamic(() => import('./PageClient'), {
   loading: () => <SignersPageSkeleton />,
 });
 
-export default async function () {
-  const tokenPrice = await getTokenPrice();
-  return <Page tokenPrice={tokenPrice} />;
+export default function () {
+  return <Page />;
 }
