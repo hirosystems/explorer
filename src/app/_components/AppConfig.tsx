f16c42022872f1ec2fcdf70d9909e3aa3ff88f09
Nicholas Barnett <n-barnett@hotmail.com>
2025-05-28 20:53:00 +0200

    feat: removed toasts

--- src/app/_components/AppConfig.tsx
+++ src/app/_components/AppConfig.tsx
@@ -1,43 +1,16 @@
 'use client';
 
-import { Box } from '@chakra-ui/react';
-import React, { useEffect } from 'react';
-import toast from 'react-hot-toast';
+import React from 'react';
 
 import { Connect } from '@stacks/connect-react';
 
 import { useAppSelector } from '../../common/state/hooks';
-import { NetworkMode } from '../../common/types/network';
-import { Text } from '../../ui/Text';
 import { selectUserSession } from '../sandbox/sandbox-slice';
 
 export const AppConfig: React.FC<{
-  queryNetworkMode: NetworkMode;
-  queryApiUrl?: string;
-  querySubnet?: string;
   children: React.ReactNode;
-}> = ({ children, queryApiUrl, queryNetworkMode, querySubnet }) => {
+}> = ({ children }) => {
   const userSession = useAppSelector(selectUserSession);
-  useEffect(() => {
-    toast(
-      <Text fontSize={'sm'}>
-        You're viewing {querySubnet ? 'a subnet' : `the ${queryNetworkMode}`}
-        {querySubnet || queryApiUrl ? (
-          <Box>
-            <br />
-            {querySubnet || queryApiUrl}
-          </Box>
-        ) : null}{' '}
-        Explorer
-      </Text>,
-      {
-        id: 'network-mode-toast',
-        style: {
-          textAlign: 'center',
-        },
-      }
-    );
-  }, []);
 
   return (
     <Connect
