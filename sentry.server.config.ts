a6940fc8e5308fb9f4f9161cccb49488ed3cbef4
He1DAr <heidarsaleh@outlook.com>
2025-06-25 15:42:53 -0400

    feat: update sentry config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,6 +2,7 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import * as Sentry from '@sentry/nextjs';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
