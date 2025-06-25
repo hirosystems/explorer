e814bd18dd876b315dde377abc5f504e6d089171
Nicholas Barnett <n-barnett@hotmail.com>
2025-06-25 16:24:40 -0500

    feat: added the events table

---
+++ src/app/txid/[txId]/redesign/events-table/EventsTable.tsx
@@ -0,0 +1,242 @@
+'use client';
+
+import { Table } from '@/common/components/table/Table';
+import { TableContainer } from '@/common/components/table/TableContainer';
+import { TableScrollIndicator } from '@/common/components/table/TableScrollIndicatorWrapper';
+import { GenericResponseType } from '@/common/hooks/useInfiniteQueryResult';
+import { THIRTY_SECONDS } from '@/common/queries/query-stale-time';
+import {
+  TxEventsQueryFilters,
+  getTxEventsByIdQueryKey,
+  useTxEventsById,
+} from '@/common/queries/useTxEventsById';
+import { microToStacksFormatted } from '@/common/utils/utils';
+import { ArrowRight } from '@phosphor-icons/react';
+import { useQueryClient } from '@tanstack/react-query';
+import { ColumnDef, PaginationState } from '@tanstack/react-table';
+import { type JSX, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+
+import { TransactionEvent, TransactionEventAssetType } from '@stacks/stacks-blockchain-api-types';
+
+import { EVENTS_TABLE_PAGE_SIZE } from './consts';
+import { EventsTableColumns } from './types';
+import {
+  getAmount,
+  getAsset,
+  getAssetType,
+  getEventType,
+  getFromAddress,
+  getToAddress,
+} from './utils';
+
+export interface EventsTableData {
+  [EventsTableColumns.Index]: number;
+  [EventsTableColumns.EventType]: TransactionEvent['event_type'];
+  [EventsTableColumns.Asset]: string;
+  [EventsTableColumns.AssetType]: TransactionEventAssetType;
+  [EventsTableColumns.Amount]: string;
+  [EventsTableColumns.From]: string;
+  [EventsTableColumns.ArrowRight]: JSX.Element;
+  [EventsTableColumns.To]: string;
+}
+
+export interface EventsTableEventColumnData {
+  eventType: TransactionEvent['event_type'];
+  asset: string;
+  assetType: TransactionEventAssetType;
+  amount: string;
+  from: string;
+  to: string;
+}
+
+export interface TxTableAddressColumnData {
+  address: string;
+  isContract: boolean;
+}
+
+export const defaultColumnDefinitions: ColumnDef<EventsTableData>[] = [
+  {
+    id: EventsTableColumns.Index,
+    header: 'Index',
+    accessorKey: EventsTableColumns.Index,
+    cell: info => info.getValue() as number,
+    enableSorting: false,
+  },
+  {
+    id: EventsTableColumns.EventType,
+    header: 'Type',
+    accessorKey: EventsTableColumns.EventType,
+    cell: info => info.getValue() as string,
+    enableSorting: false,
+  },
+  {
+    id: EventsTableColumns.Asset,
+    header: 'Asset',
+    accessorKey: EventsTableColumns.Asset,
+    cell: info => info.getValue() as string,
+    enableSorting: false,
+  },
+  {
+    id: EventsTableColumns.AssetType,
+    header: 'Asset Type',
+    accessorKey: EventsTableColumns.AssetType,
+    cell: info => info.getValue() as string,
+    enableSorting: false,
+  },
+  {
+    id: EventsTableColumns.Amount,
+    header: 'Amount',
+    accessorKey: EventsTableColumns.Amount,
+    cell: info => info.getValue() as string,
+    enableSorting: false,
+  },
+  {
+    id: EventsTableColumns.From,
+    header: 'From',
+    accessorKey: EventsTableColumns.From,
+    cell: info => info.getValue() as string,
+    enableSorting: false,
+  },
+  {
+    id: EventsTableColumns.ArrowRight,
+    header: '',
+    accessorKey: EventsTableColumns.ArrowRight,
+    cell: info => info.getValue() as JSX.Element,
+    enableSorting: false,
+  },
+  {
+    id: EventsTableColumns.To,
+    header: 'To',
+    accessorKey: EventsTableColumns.To,
+    cell: info => info.getValue() as string,
+    enableSorting: false,
+  },
+];
+
+export interface EventsTableProps {
+  txId: string;
+  initialData: GenericResponseType<EventsTableData> | undefined;
+  disablePagination?: boolean;
+  columnDefinitions?: ColumnDef<EventsTableData>[];
+  pageSize?: number;
+  filters?: TxEventsQueryFilters;
+}
+
+const DEFAULT_FILTERS: TxEventsQueryFilters = {
+  address: '',
+  type: [],
+};
+
+export function EventsTable({
+  txId,
+  filters = DEFAULT_FILTERS,
+  initialData,
+  disablePagination = false,
+  columnDefinitions,
+  pageSize = EVENTS_TABLE_PAGE_SIZE,
+}: EventsTableProps) {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize,
+  });
+
+  const handlePageChange = useCallback((page: PaginationState) => {
+    setPagination(prev => ({
+      ...prev,
+      pageIndex: page.pageIndex,
+    }));
+    window?.scrollTo(0, 0); // Smooth scroll to top
+  }, []);
+
+  const queryClient = useQueryClient();
+
+  const isCacheSetWithInitialData = useRef(false);
+
+  /**
+   * HACK: react query's cache is taking precedence over the initial data, which is causing hydration errors
+   * Setting the gcTime to 0 prevents this from happening but it also prevents us from caching requests as the user paginates through the table
+   * React query's initial data prop does not behave as expected. While it enables us to use the initial data for the first page, the initial data prop makes the logic required to replace initial data when it becomes stale difficult
+   * By explicitly setting the cache for the first page with initial data, we guarantee the table will use the initial data from the server and behave as expected
+   */
+  if (isCacheSetWithInitialData.current === false && initialData) {
+    const queryKey = getTxEventsByIdQueryKey(
+      pagination.pageSize,
+      pagination.pageIndex * pagination.pageSize,
+      txId,
+      { ...filters }
+    );
+    queryClient.setQueryData(queryKey, initialData);
+    isCacheSetWithInitialData.current = true;
+  }
+
+  // fetch data
+  let { data, isFetching, isLoading } = useTxEventsById(
+    pagination.pageSize,
+    pagination.pageIndex * pagination.pageSize,
+    txId,
+    { ...filters },
+    {
+      staleTime: THIRTY_SECONDS,
+      gcTime: THIRTY_SECONDS,
+    }
+  );
+
+  console.log('data', data);
+
+  // Reset pagination when filters change
+  useEffect(() => {
+    setPagination(prev => ({
+      ...prev,
+      pageIndex: 0,
+    }));
+  }, [filters]);
+
+  const { total, results: events = [] } = data || {};
+  const isTableFiltered = Object.values(filters).some(v => v != null && v !== '');
+
+  const rowData: EventsTableData[] = useMemo(
+    () =>
+      events.map((event, index) => {
+        const to = getToAddress(event);
+        const from = getFromAddress(event);
+        const amount = getAmount(event);
+        const asset = getAsset(event);
+        const eventType = getEventType(event);
+        const assetType = getAssetType(event);
+
+        return {
+          [EventsTableColumns.Index]: index,
+          [EventsTableColumns.EventType]: eventType,
+          [EventsTableColumns.Asset]: asset,
+          [EventsTableColumns.AssetType]: assetType,
+          [EventsTableColumns.Amount]: microToStacksFormatted(amount),
+          [EventsTableColumns.From]: from,
+          [EventsTableColumns.ArrowRight]: <ArrowRight />,
+          [EventsTableColumns.To]: to,
+        };
+      }),
+    [events]
+  );
+
+  return (
+    <Table
+      data={rowData}
+      columns={columnDefinitions ?? defaultColumnDefinitions}
+      tableContainerWrapper={table => <TableContainer minH="500px">{table}</TableContainer>}
+      scrollIndicatorWrapper={table => <TableScrollIndicator>{table}</TableScrollIndicator>}
+      pagination={
+        disablePagination
+          ? undefined
+          : {
+              manualPagination: true,
+              pageIndex: pagination.pageIndex,
+              pageSize: pagination.pageSize,
+              totalRows: total || 0,
+              onPageChange: handlePageChange,
+            }
+      }
+      isLoading={isLoading}
+      isFetching={isFetching}
+    />
+  );
+}
---
+++ src/app/txid/[txId]/redesign/events-table/EventsTableCellRenderers.tsx
@@ -0,0 +1,287 @@
+import { AddressLink, TxLink } from '@/common/components/ExplorerLinks';
+import { getTxTypeColor, getTxTypeIcon, getTxTypeLabel } from '@/common/utils/transactions';
+import {
+  formatStacksAmount,
+  getContractName,
+  microToStacksFormatted,
+  truncateHex,
+  truncateStxAddress,
+} from '@/common/utils/utils';
+import { Text, TextProps } from '@/ui/Text';
+import { Tooltip } from '@/ui/Tooltip';
+import ClarityIcon from '@/ui/icons/ClarityIcon';
+import MicroStxIcon from '@/ui/icons/MicroStxIcon';
+import StacksIconThin from '@/ui/icons/StacksIconThin';
+import { Flex, Icon } from '@chakra-ui/react';
+import { Clock, Question, XCircle } from '@phosphor-icons/react';
+
+import {
+  MempoolTransactionStatus,
+  Transaction,
+  TransactionStatus,
+} from '@stacks/stacks-blockchain-api-types';
+
+import { TxTableAddressColumnData, TxTableTransactionColumnData } from './TxsTable';
+
+const EllipsisText = ({ children, ...textProps }: { children: React.ReactNode } & TextProps) => {
+  return (
+    <Text
+      whiteSpace="nowrap"
+      overflow="hidden"
+      textOverflow="ellipsis"
+      fontSize="sm"
+      {...textProps}
+    >
+      {children}
+    </Text>
+  );
+};
+
+export const EventTypeCellRenderer = ({ eventType }: { eventType: string }) => {
+  return (
+    <Badge>
+      
+    </Badge>
+    <Flex
+      py={1}
+      pl={1}
+      pr={1.5}
+      bg="surfaceSecondary"
+      w="fit-content"
+      alignItems="center"
+      gap={1.5}
+      border="1px solid var(--stacks-colors-redesign-border-secondary)"
+      borderRadius="redesign.md"
+    >
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        p={1}
+        borderRadius="redesign.sm"
+        bg={getTxTypeColor(txType)}
+      >
+        <Icon h={3} w={3} color="neutral.sand-1000">
+          {getEventTypeIcon(txType)}
+        </Icon>
+      </Flex>
+      <Text textStyle="text-medium-xs" color="textPrimary" whiteSpace="nowrap">
+        {getEventTypeLabel(txType)}
+      </Text>
+    </Flex>
+  );
+};
+
+export const TxLinkCellRenderer = (value: string) => {
+  return (
+    <TxLink txId={value} variant="tableLink">
+      <EllipsisText>{truncateHex(value, 4, 5, false)}</EllipsisText>
+    </TxLink>
+  );
+};
+
+export const AddressLinkCellRenderer = (value: TxTableAddressColumnData) => {
+  const { address, isContract } = value;
+  return address && isContract ? (
+    <Flex
+      gap={1}
+      alignItems="center"
+      bg="surfacePrimary"
+      borderRadius="redesign.md"
+      py={0.5}
+      px={1}
+      w="fit-content"
+      _groupHover={{
+        bg: 'surfaceTertiary',
+      }}
+    >
+      <Icon h={3} w={3} color="iconPrimary">
+        <ClarityIcon />
+      </Icon>
+      <AddressLink principal={address} variant="tableLink">
+        <EllipsisText
+          textStyle="text-regular-xs"
+          color="textPrimary"
+          _hover={{
+            color: 'textInteractiveHover',
+          }}
+          fontFamily="var(--font-matter-mono)"
+        >
+          {getContractName(address)}
+        </EllipsisText>
+      </AddressLink>
+    </Flex>
+  ) : address && !isContract ? (
+    <AddressLink principal={address} variant="tableLink">
+      <EllipsisText fontSize="sm">{truncateStxAddress(address)}</EllipsisText>
+    </AddressLink>
+  ) : (
+    <EllipsisText fontSize="sm">-</EllipsisText>
+  );
+};
+
+export const FeeCellRenderer = (value: string) => {
+  const stx = microToStacksFormatted(value);
+  const microStx = formatStacksAmount(value);
+
+  return (
+    <Flex alignItems="center" gap={1}>
+      <Icon h={3} w={3} color="textSecondary">
+        {stx.length > microStx.length ? <MicroStxIcon /> : <StacksIconThin />}
+      </Icon>
+      <EllipsisText fontSize="sm">
+        {stx.length > microStx.length ? `${microStx} µSTX` : `${stx} STX`}
+      </EllipsisText>
+    </Flex>
+  );
+};
+
+export const AmountCellRenderer = (value: number) => {
+  return (
+    <Flex alignItems="center" gap={1}>
+      <Icon h={3} w={3} color="textSecondary">
+        <StacksIconThin />
+      </Icon>
+      <EllipsisText fontSize="sm">{value} STX</EllipsisText>
+    </Flex>
+  );
+};
+
+export const TimeStampCellRenderer = (value: string, tooltip?: string) => {
+  const content = (
+    <Flex
+      alignItems="center"
+      bg="surfacePrimary"
+      borderRadius="md"
+      py={0.5}
+      px={1}
+      w="fit-content"
+      _groupHover={{
+        bg: 'surfaceTertiary',
+      }}
+    >
+      <EllipsisText
+        fontSize="xs"
+        fontFamily="var(--font-matter-mono)"
+        suppressHydrationWarning={true}
+      >
+        {value}
+      </EllipsisText>
+    </Flex>
+  );
+
+  if (tooltip) {
+    return <Tooltip content={tooltip}>{content}</Tooltip>;
+  }
+
+  return content;
+};
+
+export const IconCellRenderer = (value: React.ReactNode) => {
+  return (
+    <Icon h={3} w={3} color="textSecondary">
+      {value}
+    </Icon>
+  );
+};
+
+function getTxStatusIcon(status: TransactionStatus | MempoolTransactionStatus) {
+  switch (status) {
+    case 'pending':
+      return <Clock />;
+    case 'abort_by_post_condition':
+      return <XCircle />;
+    case 'abort_by_response':
+      return <XCircle />;
+    default:
+      return <Question />;
+  }
+}
+
+function getTxStatusLabel(status: TransactionStatus | MempoolTransactionStatus) {
+  switch (status) {
+    case 'pending':
+      return 'Pending';
+    case 'abort_by_post_condition':
+      return 'Failed';
+    case 'abort_by_response':
+      return 'Failed';
+    default:
+      return 'Unknown';
+  }
+}
+
+function getTxStatusIconColor(status: TransactionStatus | MempoolTransactionStatus) {
+  switch (status) {
+    case 'pending':
+      return 'transactionStatus.pending';
+    case 'abort_by_post_condition':
+      return 'feedback.red-600';
+    case 'abort_by_response':
+      return 'feedback.red-600';
+    default:
+      return 'iconSecondary';
+  }
+}
+
+function getTxStatusBgColor(status: TransactionStatus | MempoolTransactionStatus) {
+  switch (status) {
+    case 'pending':
+      return 'transactionStatus.pending';
+    case 'abort_by_post_condition':
+      return 'transactionStatus.failed';
+    case 'abort_by_response':
+      return 'transactionStatus.failed';
+    default:
+      return 'surfaceSecondary';
+  }
+}
+
+const StatusTag = ({ status }: { status: TransactionStatus | MempoolTransactionStatus }) => {
+  return (
+    <Flex
+      alignItems="center"
+      gap={1}
+      px={1.5}
+      py={1}
+      bg={getTxStatusBgColor(status)}
+      borderRadius="redesign.md"
+    >
+      <Icon h={3} w={3} color={getTxStatusIconColor(status)}>
+        {getTxStatusIcon(status)}
+      </Icon>
+    </Flex>
+  );
+};
+
+const txTextMap: Record<Transaction['tx_type'], (data: TxTableTransactionColumnData) => string> = {
+  contract_call: (data: TxTableTransactionColumnData) => data.functionName ?? '',
+  token_transfer: (data: TxTableTransactionColumnData) => `Send ${data.amount} STX`,
+  tenure_change: (data: TxTableTransactionColumnData) =>
+    `Tenure ${data.tenureChangePayload?.cause} (#${data.blockHeight})`,
+  coinbase: (data: TxTableTransactionColumnData) => `Block #${data.blockHeight}`,
+  smart_contract: (data: TxTableTransactionColumnData) =>
+    getContractName(data.smartContract?.contractId ?? ''),
+  poison_microblock: (data: TxTableTransactionColumnData) => '',
+};
+
+export const TransactionTitleCellRenderer = (value: TxTableTransactionColumnData) => {
+  const { txType, status, txId } = value;
+
+  const text = txType ? txTextMap[txType as keyof typeof txTextMap](value) : '';
+
+  let content = (
+    <TxLink txId={txId} variant="tableLink">
+      <EllipsisText textStyle="text-medium-sm">{text}</EllipsisText>
+    </TxLink>
+  );
+
+  if (status && status !== 'success') {
+    return (
+      <Flex alignItems="center" gap={1.5}>
+        {content}
+        <StatusTag status={status} />
+      </Flex>
+    );
+  }
+  return content;
+};
