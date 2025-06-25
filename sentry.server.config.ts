92576812036d21a049b261901f1382abbf80720d
He1DAr <97111756+He1DAr@users.noreply.github.com>
2025-06-25 16:12:08 -0400

    feat: update sentry config (#2379)

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,6 +5,9 @@ import * as Sentry from '@sentry/nextjs';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
 
@@ -16,7 +19,4 @@ Sentry.init({
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
 });
