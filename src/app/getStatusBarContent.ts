9e2472c249d271492f71d20f7c33856925569795
He1DAr <97111756+He1DAr@users.noreply.github.com>
2025-07-08 20:48:01 -0400

    feat: increase cms revalidate time (#2390)

--- src/app/getStatusBarContent.ts
+++ src/app/getStatusBarContent.ts
@@ -4,7 +4,7 @@ import { IncidentContent } from '../common/types/incidents';
 export async function getStatusBarContent(): Promise<IncidentContent | null> {
   return CMS_URL
     ? fetch(CMS_URL, {
-        next: { revalidate: 60 }, // Revalidate every 1 minute
+        next: { revalidate: 60 * 4 }, // Revalidate every 4 minutes
       }).then(res => res.json())
     : Promise.resolve(null);
 }
