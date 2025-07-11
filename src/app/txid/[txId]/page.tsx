3d300d25bdebe1bbda0187c343622d62d889ec5f
Nicholas Barnett <n-barnett@hotmail.com>
2025-07-11 16:24:35 -0500

    feat: wp

--- src/app/txid/[txId]/page.tsx
+++ src/app/txid/[txId]/page.tsx
@@ -6,8 +6,20 @@ import { TxIdPageDataProvider } from './TxIdPageContext';
 export default async function Page(props: { params: Promise<{ txId: string }> }) {
   const params = await props.params;
   const { txId } = params;
-  const tokenPrice = await getTokenPrice();
-  console.log("Tx Id Page Server component", txId)
+  console.log('Tx Id Page Server component', txId);
+  let tokenPrice = {
+    stxPrice: 0,
+    btcPrice: 0,
+  };
+  try {
+    tokenPrice = await getTokenPrice();
+  } catch (error) {
+    console.error('Error fetching token price', error);
+    tokenPrice = {
+      stxPrice: 0,
+      btcPrice: 0,
+    };
+  }
 
   return (
     <TxIdPageDataProvider stxPrice={tokenPrice.stxPrice}>
