ad5209058451c755562a9f68d24885604c1f6767
Nicholas Barnett <n-barnett@hotmail.com>
2025-07-18 14:42:08 -0500

    feat: wp

--- src/api/stacksAPIFetch.ts
+++ src/api/stacksAPIFetch.ts
@@ -1,12 +1,10 @@
-// src/common/utils/api-fetch.ts
 export async function stacksAPIFetch(url: string, options: RequestInit = {}) {
-    const headers = new Headers(options.headers || {});
-    
-    // Add API key if needed
-    headers.set('x-api-key', process.env.EXPLORER_STACKS_API_KEY || '');
-    
-    return fetch(url, {
-      ...options,
-      headers,
-    });
-  }
+  const headers = new Headers(options.headers || {});
+
+  headers.set('x-api-key', process.env.EXPLORER_STACKS_API_KEY || '');
+
+  return fetch(url, {
+    ...options,
+    headers,
+  });
+}
