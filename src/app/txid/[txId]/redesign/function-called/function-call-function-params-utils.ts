41fe695bb957fc8f756a84393b8b8e5238dd33e5
Nicholas Barnett <n-barnett@hotmail.com>
2025-09-11 10:39:04 -0500

    chore: deploying main

--- src/app/txid/[txId]/redesign/function-called/function-call-function-params-utils.ts
+++ src/app/txid/[txId]/redesign/function-called/function-call-function-params-utils.ts
@@ -40,6 +40,7 @@ export function processFunctionParameters(
   fnAbi: ClarityAbiFunction
 ): Record<string, ClarityValue> {
   const final: Record<string, ClarityValue> = {};
+  const removeThis = 'remove this';
 
   Object.keys(functionParameters).forEach(arg => {
     const type = fnAbi.args.find(({ name }) => name === arg)?.type;
